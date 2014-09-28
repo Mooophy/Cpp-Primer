1da545fbae3eb09331a5fe195f1e8d34ece574da
pezy <urbancpz@gmail.com>
2014-09-28 13:13:02 +0800

    Update Format of code.

--- ch04/ex4_28.cpp
+++ ch04/ex4_28.cpp
@@ -1,9 +1,9 @@
 /*
-	Name: Exercise 4.28
-	Copyright: 
-	Author: pezy
-	Date: 28/09/14 13:08
-	Description:  Write a program to print the size of each of the built-in types.
+    Name: Exercise 4.28
+    Copyright: 
+    Author: pezy
+    Date: 28/09/14 13:08
+    Description:  Write a program to print the size of each of the built-in types.
 */
 
 
@@ -14,20 +14,20 @@ using std::cout; using std::endl;
 int main()
 {
     cout << "bool\t\tis "      << sizeof(bool)        << "bytes." << endl;
-	cout << "char\t\tis "      << sizeof(char)        << "bytes." << endl;
-	cout << "wchar_t\t\tis "   << sizeof(wchar_t)     << "bytes." << endl;
-	cout << "char16_t\tis "    << sizeof(char16_t)    << "bytes." << endl;
-	cout << "char32_t\tis "    << sizeof(char32_t)    << "bytes." << endl;
+    cout << "char\t\tis "      << sizeof(char)        << "bytes." << endl;
+    cout << "wchar_t\t\tis "   << sizeof(wchar_t)     << "bytes." << endl;
+    cout << "char16_t\tis "    << sizeof(char16_t)    << "bytes." << endl;
+    cout << "char32_t\tis "    << sizeof(char32_t)    << "bytes." << endl;
     cout << "short\t\tis "     << sizeof(short)       << "bytes." << endl;
-	cout << "int\t\tis "       << sizeof(int)         << "bytes." << endl;
-	cout << "long\t\tis "      << sizeof(long)        << "bytes." << endl;
-	cout << "long long\tis "   << sizeof(long long)   << "bytes." << endl;
-  	cout << "float\t\tis "     << sizeof(float)       << "bytes." << endl;
-  	cout << "double\t\tis "    << sizeof(double)      << "bytes." << endl;
-  	cout << "long double\tis " << sizeof(long double) << "bytes." << endl;
-	cout << endl;
-	
-	return 0;
+    cout << "int\t\tis "       << sizeof(int)         << "bytes." << endl;
+    cout << "long\t\tis "      << sizeof(long)        << "bytes." << endl;
+    cout << "long long\tis "   << sizeof(long long)   << "bytes." << endl;
+    cout << "float\t\tis "     << sizeof(float)       << "bytes." << endl;
+    cout << "double\t\tis "    << sizeof(double)      << "bytes." << endl;
+    cout << "long double\tis " << sizeof(long double) << "bytes." << endl;
+    cout << endl;
+    
+    return 0;
 }
 
 //Print
