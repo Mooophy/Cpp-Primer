64d0cbc44a24d25b4f27220b18bf0ac92490c1c6
Queequeg <lvchong24@gmail.com>
2014-09-28 18:48:26 +0800

    solution for exercise 15.42 (b)

---
+++ ch15/ex15.42_b/queryhistory.cpp
@@ -0,0 +1,11 @@
+#include"queryhistory.h"
+
+#include<memory>
+using std::shared_ptr; using std::make_shared;
+
+size_t QueryHistory::add_query(const Query &query)
+{
+	shared_ptr<Query> p = make_shared<Query>(query);
+	query_vec.push_back(p);
+	return query_vec.size() - 1;
+}
