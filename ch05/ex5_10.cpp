7c47a9d967061a3f66def9916b8c081df63e1b92
pezy <urbancpz@gmail.com>
2014-10-15 00:43:18 +0800

    Update ex5_10.cpp
    The switch statemen more better.

--- ch05/ex5_10.cpp
+++ ch05/ex5_10.cpp
@@ -2,27 +2,40 @@
 
 using std::cin; using std::cout; using std::endl;
 
-int main(void)
+int main()
 {
-	char c;
-	int aCnt = 0, eCnt = 0, iCnt = 0, oCnt = 0, uCnt = 0;
-	while (cin >> c)
-	{
-		if (c == 'a' || c == 'A')
-			++aCnt;
-		else if (c == 'e' || c == 'E')
-			++eCnt;
-		else if (c == 'i' || c == 'I')
-			++iCnt;
-		else if (c == 'o' || c == 'O')
-			++oCnt;
-		else if (c == 'u' || c == 'U')
-			++uCnt;
-	}
-	cout << "Num of vowel a(A) :  " << aCnt << endl;
-	cout << "Num of vowel e(E) :  " << eCnt << endl;
-	cout << "Num of vowel i(I) :  " << iCnt << endl;
-	cout << "Num of vowel o(O) :  " << oCnt << endl;
-	cout << "Num of vowel u(U) :  " << uCnt << endl;
-	return 0;
+    unsigned aCnt = 0, eCnt = 0, iCnt = 0, oCnt = 0, uCnt = 0;
+    char ch;
+    while (cin >> ch)
+        switch (ch)
+        {
+            case 'a':
+            case 'A':
+                ++aCnt;
+                break;
+            case 'e':
+            case 'E':
+                ++eCnt;
+                break;
+            case 'i':
+            case 'I':
+                ++iCnt;
+                break;
+            case 'o':
+            case 'O':
+                ++oCnt;
+                break;
+            case 'u':
+            case 'U':
+                ++uCnt;
+                break;
+        }
+    
+    cout << "Number of vowel a(A): \t" << aCnt << '\n'
+        << "Number of vowel e(E): \t" << eCnt << '\n'
+        << "Number of vowel i(I): \t" << iCnt << '\n'
+        << "Number of vowel o(O): \t" << oCnt << '\n'
+        << "Number of vowel u(U): \t" << uCnt << endl;
+    
+    return 0;
 }
