4e7e2c743ec79260178b2380847462a9dec28db6
pezy <urbancpz@gmail.com>
2014-10-16 15:47:36 +0800

    Fixed the print error when no word repeated.

--- ch05/ex5_20.cpp
+++ ch05/ex5_20.cpp
@@ -1,38 +1,19 @@
-//!@Alan
-//!
-//! Exercise 5.20:
-//! Write a program to read a sequence of strings from the standard
-//! input until either the same word occurs twice in succession or
-//! all the words have been read. Use a while loop to read the text
-//! one word at a time. Use the break statement to terminate the loop
-//! if a word occurs twice in succession. Print the word if it occurs
-//! twice in succession, or else print a message saying that no word
-//! was repeated.
-//!
-
 #include <iostream>
 #include <string>
-#include <vector>
 
-using namespace std;
+using std::cout; using std::cin; using std::endl; using std::string;
 
 int main()
 {
-    string current_str, previous_str;
-
-    cout<<"Please Enter:\n";
-    while(cin>>current_str)
-    {
-        if(current_str.compare(previous_str)==0)
+    string read, tmp;
+    while (cin >> read)
+        if (read == tmp)
         {
+            cout << read << " occurs twice in succession." << endl;
             break;
         }
-        previous_str = current_str;
-    }
-
-    cout<<"The repeated word is "
-          <<previous_str
-            <<std::endl;
-
+        else tmp = read;
+    
+    if (cin.eof()) cout << "no word was repeated." << endl;
     return 0;
 }
