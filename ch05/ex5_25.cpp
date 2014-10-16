626d319a66322530c32b604f42989c1278f92b44
pezy <urbancpz@gmail.com>
2014-10-16 16:27:05 +0800

    Update ex5_25.cpp
    Doing many works in catch scope is not a good habit.

--- ch05/ex5_25.cpp
+++ ch05/ex5_25.cpp
@@ -1,33 +1,21 @@
 #include <iostream>
 #include <stdexcept>
-
-using namespace std;
+using std::cin; using std::cout; using std::endl; using std::runtime_error;
 
 int main(void)
 {
 	int a, b;
-	cout << "Enter two numbers :" << endl;
-
+	cout << "Input two integers: ";
 	while (cin >> a >> b)
 	{
-		try{
-			if (b == 0)
-				throw runtime_error("divisor is 0");
-			cout << static_cast<double>(a) / b << endl;
-			break;
-		}
-		catch (runtime_error err){
-			cout << err.what()
-				<< "\nTry again ? Enter y or n" << endl;
-			char c;
-			cin >> c;
-			if (!cin || c == 'n')
-				break;
-			else
-			{
-				cout << "Enter two numbers :" << endl;
-			}
-		}
+	    try {
+	        if (b == 0) throw runtime_error("divisor is 0");
+	        cout << static_cast<double>(a) / b << endl;
+	        cout << "Input two integers: ";
+	    } catch (runtime_error err) {
+	        cout << err.what() << "\n"
+	             << "Try again.\nInput two integers: ";
+	    }
 	}
 
 	return 0;
