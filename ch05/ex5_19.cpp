12972ba7fb997839b47c3e73231f51198810291e
pezy <urbancpz@gmail.com>
2014-10-16 15:26:55 +0800

    Create ex5_19.cpp

---
+++ ch05/ex5_19.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using std::cout; using std::cin; using std::endl; using std::string;
+
+int main()
+{
+    string rsp;
+    do {
+        cout << "Input two strings: ";
+        string str1, str2;
+        cin >> str1 >> str2;
+        cout << (str1 <= str2 ? str1 : str2) 
+             << " is less than the other. " << "\n\n"
+             << "More? Enter yes or no: ";
+        cin >> rsp;
+    } while (!rsp.empty() && rsp[0] == 'y');
+    return 0;
+}
