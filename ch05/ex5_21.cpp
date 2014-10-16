a844d134229bc4bd34f070b44401e5f476d0f3d3
pezy <urbancpz@gmail.com>
2014-10-16 15:55:49 +0800

    Create ex5_21.cpp

---
+++ ch05/ex5_21.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using std::cout; using std::cin; using std::endl; using std::string;
+
+int main()
+{
+    string read, tmp;
+    while (cin >> read)
+        if (!isupper(read[0])) continue;
+        else if (read == tmp) break;
+        else tmp = read;
+    
+    if (cin.eof()) cout << "no word was repeated." << endl;
+    else cout << read << " occurs twice in succession." << endl;
+    return 0;
+}
