9657420405d1157eedddd50246a717b293677d2c
pezy <urbancpz@gmail.com>
2014-10-16 15:49:54 +0800

    more terse.

--- ch05/ex5_20.cpp
+++ ch05/ex5_20.cpp
@@ -7,13 +7,10 @@ int main()
 {
     string read, tmp;
     while (cin >> read)
-        if (read == tmp)
-        {
-            cout << read << " occurs twice in succession." << endl;
-            break;
-        }
+        if (read == tmp) break;
         else tmp = read;
     
     if (cin.eof()) cout << "no word was repeated." << endl;
+    else cout << read << " occurs twice in succession." << endl;
     return 0;
 }
