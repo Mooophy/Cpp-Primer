c424dcd70f4e63be8b7d4429a9fa901384b10f99
pezy <urbancpz@gmail.com>
2014-10-17 14:37:26 +0800

    Create fact.cc

---
+++ ch06/fact.cc
@@ -0,0 +1,17 @@
+#include "Chapter6.h"
+#include <iostream>
+
+int fact(int val)
+{
+    if (val == 0 || val == 1) return 1;
+    else return val * fact(val-1);
+}
+
+int func()
+{
+    int n, ret = 1;
+    std::cout << "input a number: ";
+    std::cin >> n;
+    while (n > 1) ret *= n--;
+    return ret;
+}
