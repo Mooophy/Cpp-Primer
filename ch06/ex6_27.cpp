3734fcf557a1867dd098423ac206c8bca64eb3c1
pezy <urbancpz@gmail.com>
2014-10-23 22:24:53 +0800

    Update ex6_27.cpp
    "sum" is more better name of the function, and the range for loop use "const auto&" more better.

--- ch06/ex6_27.cpp
+++ ch06/ex6_27.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
 #include <initializer_list>
 
-using namespace std;
-
-int add(initializer_list<const int> il);
+int sum(std::initializer_list<int> il)
+{
+    int sum = 0;
+    for (const auto &i : il)
+        sum += i;
+    return sum;
+}
 
 int main(void)
 {
-	int result = add({ 1, 2, 3, 4, 5 });
-	
-	cout << result << endl;
-
-	return 0;
-}
-
-int add(initializer_list<const int> il)
-{
-	int sum = 0;
-	for (auto a : il)
-		sum += a;
-	return sum;
+    std::cout << sum({1,2,3,4,5}) << std::endl;
+    return 0;
 }
