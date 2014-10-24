74c1b3b8fa0789226018008beef30c00ce34a963
Huang Bo <everiamhere@163.com>
2014-10-24 20:53:46 +0800

    Update Item_data.cpp
    Add some primary comments.

--- ch02/Item_data.cpp
+++ ch02/Item_data.cpp
@@ -2,24 +2,28 @@
 
 Book_data trans, total;
 
-int main()
+int Main()
 {
-	total.GetData();
+  // Get the first data from input
+    total.GetData();
 
-	while (trans.GetData())
-	{
+  // Get the following data from input
+    while (trans.GetData())
+    {
+      // If the following data is same book with the previous one, then calculate the revenue and aver. price
+        if (trans.book_no == total.book_no)
+        {
+            total.revenue += trans.revenue;
+            total.sale_num += trans.sale_num;
+        }
+        // If not the same book, show the sum and aver. result of last book
+        // and assign the different new book info to total object
+        else
+        {
+            total.Show_data();
+            total.Set_data(trans);
+        }
+    }
 
-		if (trans.book_no == total.book_no)
-		{
-			total.revenue += trans.revenue;
-			total.sale_num += trans.sale_num;
-		}
-		else
-		{
-			total.Show_data();
-			total.Set_data(trans);
-		}
-	}
-
-	return 0;
+    return 0;
 }
