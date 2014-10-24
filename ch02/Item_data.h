42e2f6f8645f224410a9b84aae9832f09714e39a
Huang Bo <everiamhere@163.com>
2014-10-24 20:53:08 +0800

    Update Item_data.h
    Add simple comments

--- ch02/Item_data.h
+++ ch02/Item_data.h
@@ -7,58 +7,60 @@ using namespace std;
 
 struct Book_data
 {
-	string book_no;
-	unsigned sale_num;
-	float price;
-	double revenue;
+    string book_no;
+    unsigned sale_num;
+    float price;
+    double revenue;
 
-	int GetData()
-	{
-		cout << "Book No.: " << endl;
-		cin >> book_no;
-		cout << "sale num.: " << endl;
-		cin >> sale_num;
-		cout << "Book price.: " << endl;
-		cin >> price;
-		cout << "--------------------------------------------------------------" << endl;
-		revenue = price * sale_num;
+  // Get input data from CLI
+    int GetData()
+    {
+        cout << "Book No.: " << endl;
+        cin >> book_no;
+        cout << "sale num.: " << endl;
+        cin >> sale_num;
+        cout << "Book price.: " << endl;
+        cin >> price;
+        cout << "--------------------------------------------------------------" << endl;
+        revenue = price * sale_num;
 
-		if (book_no != "")
-		{
-			return 1;
-		}
-		else
-		{
-			return 0;
-		}
-	}
+        if (book_no != "")
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 
-	int Show_data()
-	{
-		cout << "Book No.\tSold Number\tTotal\tAve. Price" << endl;
-		cout << book_no << "\t\t" << sale_num << "\t\t" <<
-			revenue << "\t" << revenue / sale_num << endl;
-		cout << "================================================================" << endl;
+  // Show book sale data to CLI.
+    int Show_data()
+    {
+        cout << "Book No.\tSold Number\tTotal\tAve. Price" << endl;
+        cout << book_no << "\t\t" << sale_num << "\t\t" <<
+            revenue << "\t" << revenue / sale_num << endl;
+        cout << "================================================================" << endl;
 
-		return 1;
-	}
+        return 1;
+    }
 
-	int Set_data(Book_data src)
-	{
-		if (src.book_no != "")
-		{
-			book_no = src.book_no;
-			price = src.price;
-			sale_num = src.sale_num;
-			revenue = src.revenue;
+  // Set values to members of Book_data object
+    int Set_data(Book_data src)
+    {
+        if (src.book_no != "")
+        {
+            book_no = src.book_no;
+            price = src.price;
+            sale_num = src.sale_num;
+            revenue = src.revenue;
 
-			return 1;
-		}
-		else
-		{
-			return 0;
-		}
-	}
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 };
-
 #endif
