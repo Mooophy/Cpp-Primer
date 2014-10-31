2414003f2360ff13b19ba51c9a03d2644d4e6266
Huang Bo <everiamhere@163.com>
2014-10-31 13:14:02 +0800

    Update ex5_14.cpp
    In the previous version, when you successively input same strings, then end the loop by ctrl+z(windows), the result is no duplicated words, which is not accord with our input.(eg. input three "123" and then ended with ctrl+z). 
    The reason is that if the inputs are same from start, we will never enter the else branch where assign value to variable 'max_duplicated'.
    So the operation on 'max_duplicated' should be done only when the 'count > max_duplicated.second + 1', which shows that continously duplicated inputs are tested. And when the input is not same with the previous, we should set the count for the new string to 1.

--- ch05/ex5_14.cpp
+++ ch05/ex5_14.cpp
@@ -3,19 +3,22 @@
 
 using std::cout; using std::cin; using std::endl; using std::string; using std::pair;
 
-int main()
-{ 
-    pair<string, int> max_duplicated;
-    int count = 0;
-    for (string str, prestr; cin >> str; prestr = str)
-    {
-        if (str == prestr) { ++count; continue; }
-        else if (count > max_duplicated.second) max_duplicated = {prestr, count};
-        count = 0;
-    }
-    
-    if (max_duplicated.first.empty()) cout << "There's no duplicated string." << endl;
-    else cout << "the word " << max_duplicated.first << " occurred " << max_duplicated.second+1 << " times. " << endl;
-    
-    return 0;
-} 
+int main() {
+
+	pair<string, int> max_duplicated;
+	int count = 1;
+	for (string str, prestr; cin >> str; prestr = str)
+	{
+		if (str == prestr) { ++count;}
+		else count = 1;
+
+		if (count > max_duplicated.second + 1) {
+			max_duplicated = { prestr, count };
+		}
+	}
+
+	if (max_duplicated.first.empty()) cout << "There's no duplicated string." << endl;
+	else cout << "the word " << max_duplicated.first << " occurred " << max_duplicated.second << " times. " << endl;
+
+	return 0;
+}
