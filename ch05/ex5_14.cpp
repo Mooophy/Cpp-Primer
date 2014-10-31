5c01a82268c25a494e193466663ce80982274900
Huang Bo <everiamhere@163.com>
2014-10-31 13:17:47 +0800

    Update ex5_14.cpp
    Adjust the format for code comparison.

--- ch05/ex5_14.cpp
+++ ch05/ex5_14.cpp
@@ -3,22 +3,22 @@
 
 using std::cout; using std::cin; using std::endl; using std::string; using std::pair;
 
-int main() {
+int main()
+{ 
+    pair<string, int> max_duplicated;
+    int count = 0;
+    for (string str, prestr; cin >> str; prestr = str)
+    {
+	if (str == prestr) { ++count;}
+	else count = 1;
 
-	pair<string, int> max_duplicated;
-	int count = 1;
-	for (string str, prestr; cin >> str; prestr = str)
-	{
-		if (str == prestr) { ++count;}
-		else count = 1;
-
-		if (count > max_duplicated.second + 1) {
-			max_duplicated = { prestr, count };
-		}
+	if (count > max_duplicated.second + 1) {
+		max_duplicated = { prestr, count };
 	}
-
-	if (max_duplicated.first.empty()) cout << "There's no duplicated string." << endl;
-	else cout << "the word " << max_duplicated.first << " occurred " << max_duplicated.second << " times. " << endl;
-
-	return 0;
-}
+    }
+    
+    if (max_duplicated.first.empty()) cout << "There's no duplicated string." << endl;
+    else cout << "the word " << max_duplicated.first << " occurred " << max_duplicated.second << " times. " << endl;
+    
+    return 0;
+} 
