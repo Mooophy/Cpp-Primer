bd10ea39b5ba70c51e75fdb08876f7813c77c295
pezy <urbancpz@gmail.com>
2014-11-08 22:37:34 +0800

    Create ex7_06.h

---
+++ ch07/ex7_06.h
@@ -0,0 +1,54 @@
+//
+//  ex7_06.h
+//  Exercise 7.6
+//
+//  Created by pezy on 11/8/14.
+//  Copyright (c) 2014 pezy. All rights reserved.
+//
+
+#ifndef CP5_ex7_06_h
+#define CP5_ex7_06_h
+
+#include <string>
+#include <iostream>
+
+struct Sales_data {
+    std::string isbn() const { return bookNo; };
+    Sales_data& combine(const Sales_data&);
+    
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+// member functions.
+Sales_data& Sales_data::combine(const Sales_data& rhs)
+{
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+// nonmember functions
+std::istream &read(std::istream &is, Sales_data &item)
+{
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = price * item.units_sold;
+    return is;
+}
+
+std::ostream &print(std::ostream &os, const Sales_data &item)
+{
+    os << item.isbn() << " " << item.units_sold << " " << item.revenue;
+    return os;
+}
+
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs)
+{
+    Sales_data sum = lhs;
+    sum.combine(rhs);
+    return sum;
+}
+
+#endif
