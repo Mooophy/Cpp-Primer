b3ed48a32f15cf4f98ce89c2b2767e08f1fa4be8
pezy <urbancpz@gmail.com>
2014-11-09 01:01:20 +0800

    Create ex7_12.h

---
+++ ch07/ex7_12.h
@@ -0,0 +1,62 @@
+//
+//  ex7_12.h
+//  Exercise 7.12
+//
+//  Created by pezy on 11/9/14.
+//  Copyright (c) 2014 pezy. All rights reserved.
+//
+
+#ifndef CP5_ex7_12_h
+#define CP5_ex7_12_h
+
+#include <string>
+#include <iostream>
+
+struct Sales_data;
+std::istream &read(std::istream&, Sales_data&);
+
+struct Sales_data {
+    Sales_data() = default;
+    Sales_data(const std::string &s):bookNo(s) {}
+    Sales_data(const std::string &s, unsigned n, double p):bookNo(s),units_sold(n),revenue(n*p){}
+    Sales_data(std::istream &is) { read(is, *this);}
+    
+    std::string isbn() const { return bookNo; };
+    Sales_data& combine(const Sales_data&);
+    
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+// member functions.
+Sales_data& Sales_data::combine(const Sales_data& rhs)
+{
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+// nonmember functions
+std::istream &read(std::istream &is, Sales_data &item)
+{
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = price * item.units_sold;
+    return is;
+}
+
+std::ostream &print(std::ostream &os, const Sales_data &item)
+{
+    os << item.isbn() << " " << item.units_sold << " " << item.revenue;
+    return os;
+}
+
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs)
+{
+    Sales_data sum = lhs;
+    sum.combine(rhs);
+    return sum;
+}
+
+#endif
