c3e6085c318fb4fae84c549144e82c63c564bbad
pezy-pc <urbancpz@gmail.com>
2014-11-14 15:10:31 +0800

    separate the header and cpp.

--- ch07/ex7_26.cpp
+++ ch07/ex7_26.cpp
@@ -1,110 +1,41 @@
-//! @Alan
-//!
-//! Exercise 7.26:
-//! Define Sales_data::avg_price as an inline function.
-//!
-//!
+//
+//  ex7_26.cpp
+//  Exercise 7.26
+//
+//  Created by pezy on 11/9/14.
+//  Copyright (c) 2014 pezy. All rights reserved.
+//
+//  @Brief implementation of class Sales_data
+//  @See ex7_26.h
 
-#include <iostream>
-#include <string>
+#include "ex7_26.h"
 
-class wy_Sales_data
+// member functions.
+Sales_data& Sales_data::combine(const Sales_data& rhs)
 {
-    friend wy_Sales_data add    (const wy_Sales_data &lhs, const wy_Sales_data &rhs);
-    friend std::istream &read   (std::istream &is,    wy_Sales_data &sd);
-    friend std::ostream &print  (std::ostream &os, const wy_Sales_data &sd);
-
-private:
-    std::string bookNo;
-    unsigned units_sold = 0;
-    double revenue = 0.0;
-
-public:
-    //! constructors:
-    wy_Sales_data() = default;                                  //default
-    //wy_Sales_data() : units_sold(0) , revenue(0){}
-    wy_Sales_data(const std::string &str) : bookNo(str) {}      //with bookNo
-    wy_Sales_data(const std::string &str, unsigned uni, double prc) :
-        bookNo(str), units_sold(uni), revenue(uni * prc){}      //with all
-    wy_Sales_data(std::istream &is)
-    {
-        double price;
-        is >> this->bookNo >> this->units_sold >> price;
-        this->revenue = price * this->units_sold;
-    }
-
-    //! member functions:
-    std::string isbn(void) const {return this->bookNo;}
-    wy_Sales_data &combine (const wy_Sales_data &sd);
-
-    inline double avgPrice() const {return units_sold ? revenue : 0;}
-};
-
-//! friend:
-wy_Sales_data add    (const wy_Sales_data &lhs, const wy_Sales_data &rhs);
-std::istream &read   (std::istream &is,    wy_Sales_data &sd);
-std::ostream &print  (std::ostream &os, const wy_Sales_data &sd);
-
-
-int main()
-{
-    std::cout << "Please enter the first book:\n";
-    wy_Sales_data total(std::cin);
-    while(true)
-    {
-        std::cout << "Please enter the next book:\n";
-        wy_Sales_data trans(std::cin);
-
-        if(total.isbn() == trans.isbn())
-        {
-            total = add(total, trans);
-        }
-        else
-        {
-            print(std::cout,total);
-            std::cout<<"\n";
-            total = trans;
-        }
-    }
-    return 0;
-}
-
-/*
-wy_Sales_data::wy_Sales_data( std::istream &is)
-{
-    read(is,*this);
-}
-*/
-
-wy_Sales_data &wy_Sales_data::combine(const wy_Sales_data &sd)
-{
-    this->revenue += sd.revenue;
-    this->units_sold += sd.units_sold;
-
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
     return *this;
 }
 
-std::istream &read(std::istream &is, wy_Sales_data &sd)
+// friend functions
+std::istream &read(std::istream &is, Sales_data &item)
 {
-    double price;
-    is >> sd.bookNo >> sd.units_sold >> price;
-    sd.revenue = price * sd.units_sold;
-
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = price * item.units_sold;
     return is;
 }
 
-std::ostream &print(std::ostream &os, const wy_Sales_data &sd)
+std::ostream &print(std::ostream &os, const Sales_data &item)
 {
-    os << sd.isbn() << " "
-       << sd.revenue << " "
-       << sd.units_sold;
-
+    os << item.isbn() << " " << item.units_sold << " " << item.revenue;
     return os;
 }
 
-wy_Sales_data add(const wy_Sales_data &lhs, const wy_Sales_data &rhs)
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs)
 {
-    wy_Sales_data sum =lhs;
+    Sales_data sum = lhs;
     sum.combine(rhs);
     return sum;
 }
---
+++ ch07/ex7_26.h
@@ -0,0 +1,51 @@
+//
+//  ex7_26.h
+//  Exercise 7.26
+//
+//  Created by pezy on 11/14/14.
+//  Copyright (c) 2014 pezy. All rights reserved.
+//
+//  @See ex7_21.h
+//  @Add inline member function "Sales_data::avg_pric" 
+
+#ifndef CP5_ex7_26_h
+#define CP5_ex7_26_h
+
+#include <string>
+#include <iostream>
+
+class Sales_data {
+    friend std::istream &read(std::istream &is, Sales_data &item);
+    friend std::ostream &print(std::ostream &os, const Sales_data &item);
+    friend Sales_data add(const Sales_data &lhs, const Sales_data &rhs);
+
+public:
+    Sales_data() = default;
+    Sales_data(const std::string &s):bookNo(s) {}
+    Sales_data(const std::string &s, unsigned n, double p):bookNo(s),units_sold(n),revenue(n*p){}
+    Sales_data(std::istream &is) { read(is, *this); }
+
+    std::string isbn() const { return bookNo; };
+    Sales_data& combine(const Sales_data&);
+    
+private:
+    inline double avg_price() const;        
+
+private:
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+inline
+double Sales_data::avg_price() const
+{
+    return units_sold ? revenue/units_sold : 0;
+}
+
+// declarations for nonmember parts of the Sales_data interface.
+std::istream &read(std::istream &is, Sales_data &item);
+std::ostream &print(std::ostream &os, const Sales_data &item);
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs);
+
+#endif
