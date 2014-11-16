c9cf9749d85055a1f2c588e6d957ac949e09cec0
shbling <shbling@gmail.com>
2014-11-16 17:27:37 +0800

    Update ex7_23_24_25.cpp

--- ch07/ex7_23_24_25.cpp
+++ ch07/ex7_23_24_25.cpp
@@ -35,8 +35,8 @@ public:
 
     //!  constructs:
     wyScreen() = default;
-    wyScreen(const pos h, const pos w) : height(h), width(w)，contents(h * w, ' ') { }
-    wyScreen(const pos h, const pos w, const std::string &c) : height(h), width(w) contents(h * w, c) { }
+	wyScreen(const pos h, const pos w) : height(h), width(w), contents(h * w, ' ') { }
+	wyScreen(const pos h, const pos w, const char &c) : height(h), width(w), contents(h * w, c){ }
 
 private:
     pos cursor = 0;
