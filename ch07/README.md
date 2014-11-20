713ae637f80c61b3ebc66648b2616de691eb96f0
pezy-pc <urbancpz@gmail.com>
2014-11-20 18:26:20 +0800

    added 7.41~7.42

--- ch07/README.md
+++ ch07/README.md
@@ -243,3 +243,21 @@ private:
   std::string pubdate_;
 };
 ```
+
+## Exercise 7.41 [Header](ex7_41.h)|[Cpp](ex7_41.cpp)|[Test](ex7_41_TEST.cpp)
+## Exercise 7.42
+
+```cpp
+class Book {
+public:
+  Book(unsigned no, std::string name, std::string author, std::string pubdate):no_(no), name_(name), author_(author), pubdate_(pubdate) { }
+  Book() : Book(0, "", "", "") { }
+  Book(std::istream &in) : Book() { in >> no_ >> name_ >> author_ >> pubdate_; }
+
+private:
+  unsigned no_;
+  std::string name_;
+  std::string author_;
+  std::string pubdate_;
+};
+```
---
+++ ch07/ex7_41.cpp
@@ -0,0 +1,48 @@
+//
+//  ex7_41.cpp
+//  Exercise 7.41
+//
+//  Created by pezy on 11/20/14.
+//  Copyright (c) 2014 pezy. All rights reserved.
+//
+//  @Brief implementation of class Sales_data
+//  @See ex7_41.h
+
+#include "ex7_41.h"
+
+// constructor
+Sales_data::Sales_data(std::istream &is) : Sales_data()
+{
+    std::cout << "Sales_data(istream &is)" << std::endl;
+    read(is, *this);
+}
+
+// member functions.
+Sales_data& Sales_data::combine(const Sales_data& rhs)
+{
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+// friend functions
+std::istream &read(std::istream &is, Sales_data &item)
+{
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = price * item.units_sold;
+    return is;
+}
+
+std::ostream &print(std::ostream &os, const Sales_data &item)
+{
+    os << item.isbn() << " " << item.units_sold << " " << item.revenue;
+    return os;
+}
+
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs)
+{
+    Sales_data sum = lhs;
+    sum.combine(rhs);
+    return sum;
+}
