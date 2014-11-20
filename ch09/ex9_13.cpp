3b825e0a1b44da3dabba2d9cc410bb99b8f0a2d6
Yue Wang <Moophyalan@gmail.com>
2014-11-20 20:48:30 +1300

    better format

--- ch09/ex9_13.cpp
+++ ch09/ex9_13.cpp
@@ -1,4 +1,4 @@
-//! @Alan
+//! @author @shbling @Alan
 //!
 //! Exercise 9.13:
 //! How would you initialize a vector<double> from a list<int>?
@@ -9,24 +9,26 @@
 #include <string>
 #include <vector>
 #include <list>
+
 using std::list;using  std::vector;using std::cout;using std::endl;
 
 int main()
-{ 
-	list<int> ilst(5, 4);
-	vector<int> ivc(5, 5);
-	vector<double> dvc (ilst.begin(),ilst.end());//from list<int>  to  vector<double>
-	for (auto i : ilst)
-		cout << i;
-	cout << endl;
-	for (auto t : dvc)
-		cout << t;
-	cout << endl;
-	vector<double> dvc2(ivc.begin(), ivc.end());//from vector<int>   to  vector<double>
-	for (auto i : ivc)
-		cout << i;
-	cout << endl;
-	for (auto t : dvc2)
-		cout << t;
-	return 0;
+{
+    list<int> ilst(5, 4);
+    vector<int> ivc(5, 5);
+
+    //! from list<int>  to  vector<double>
+    vector<double> dvc (ilst.begin(),ilst.end());
+    for (auto i : ilst) cout << i;
+    cout << endl;
+    for (auto t : dvc)  cout << t;
+    cout << endl;
+
+    //! from vector<int>   to  vector<double>
+    vector<double> dvc2(ivc.begin(), ivc.end());
+    for (auto i : ivc)  cout << i;
+    cout << endl;
+    for (auto t : dvc2) cout << t;
+
+    return 0;
 }
