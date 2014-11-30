da093116b54ec764d95340e322bdb7057ff69ef3
shbling <shbling@gmail.com>
2014-11-30 19:24:46 +0800

    Update ex10_6_9_10.cpp

--- ch10/ex10_6_9_10.cpp
+++ ch10/ex10_6_9_10.cpp
@@ -36,7 +36,7 @@ int main()
     std::vector<int> v;
     v.resize(10);
     set0(v);
-    std::vector<int>::iterator iter=v.begin();
+    for(std::vector<int>::iterator iter=v.begin(); iter != v.cend();++iter)
     std::cout<<*iter<<std::endl;
     //! Exercise 10.9
     std::vector<std::string> vs = {"a","v","a","s","a","a","a"};
