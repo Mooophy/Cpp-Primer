1895d351ba36c9c2d10503b0c7df2761d3586d2c
pezy-pc <urbancpz@gmail.com>
2014-12-01 20:22:30 +0800

    improved formatting for ex9_13.cpp

--- ch09/ex9_13.cpp
+++ ch09/ex9_13.cpp
@@ -10,25 +10,29 @@
 #include <vector>
 #include <list>
 
-using std::list;using  std::vector;using std::cout;using std::endl;
+using std::list; using std::vector; using std::cout; using std::endl;
 
 int main()
 {
     list<int> ilst(5, 4);
     vector<int> ivc(5, 5);
 
-    //! from list<int>  to  vector<double>
-    vector<double> dvc (ilst.begin(),ilst.end());
-    for (auto i : ilst) cout << i;
+    //! from list<int> to vector<double>
+    vector<double> dvc(ilst.begin(),ilst.end());
+    for (auto i : ilst)
+        cout << i;
     cout << endl;
-    for (auto t : dvc)  cout << t;
+    for (auto t : dvc)
+        cout << t;
     cout << endl;
 
-    //! from vector<int>   to  vector<double>
+    //! from vector<int> to vector<double>
     vector<double> dvc2(ivc.begin(), ivc.end());
-    for (auto i : ivc)  cout << i;
+    for (auto i : ivc)
+        cout << i;
     cout << endl;
-    for (auto t : dvc2) cout << t;
+    for (auto t : dvc2)
+        cout << t;
 
     return 0;
 }
