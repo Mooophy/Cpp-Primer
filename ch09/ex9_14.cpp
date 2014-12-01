cc206e4d7b8ba910967c79f82e0af48c1e77d537
pezy_mbp <urbancpz@gmail.com>
2014-12-02 00:06:01 +0800

    fixed 9.14

--- ch09/ex9_14.cpp
+++ ch09/ex9_14.cpp
@@ -1,23 +1,25 @@
-//! @Alan
+//! @Alan @pezy
 //!
 //! Exercise 9.14:
 //! Write a program to assign the elements from a list of char* pointers
 //! to C-style character strings
-//  not done
-//! to a vector of strings.
 //!
+//! @Notice C-style character strings should use const char*, otherwise warning.
+//!
+
 #include <iostream>
 #include <string>
 #include <vector>
-#include <deque>
 #include <list>
-
 
 int main()
 {
-    std::list<char*> l(10);
+    std::list<const char*> l{"Mooophy", "pezy", "Queeuqueg"};
     std::vector<std::string> v;
     v.assign(l.cbegin(), l.cend());
+
+    for (const auto &ch : v)
+        std::cout << ch << std::endl;
 
     return 0;
 }
