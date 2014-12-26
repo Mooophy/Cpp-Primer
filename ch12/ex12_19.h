0cbce96ac167989aa79d153683a239a5c0d92127
pezy-pc <urbancpz@gmail.com>
2014-12-26 18:51:50 +0800

    added 12_19

---
+++ ch12/ex12_19.h
@@ -0,0 +1,81 @@
+//
+//  ex12_19.cpp
+//  Exercise 12.19 
+//
+//  Created by pezy on 12/26/14.
+//  Copyright (c) 2014 pezy. All rights reserved.
+//
+//  Define your own version of StrBlobPtr and 
+//  update your StrBlob class with the appropriate friend declaration and begin and end members. 
+//
+//  @See    ex12_02.h
+
+#include <vector>
+#include <string>
+#include <initializer_list>
+#include <memory>
+#include <exception>
+
+using std::vector; using std::string;
+
+class StrBlobPtr {
+public:
+    StrBlobPtr():curr(0) { }
+    StrBlobPtr(StrBlob &a, size_t sz = 0):wptr(a.data), curr(sz) { }
+    string& deref() const {
+        auto p = check(curr, "dereference past end");
+        return (*p)[curr];
+    }
+    StrBlobPtr& incr() {
+        check(curr, "increment past end of StrBlobPtr");
+        ++curr;
+        return *this;
+    }
+    
+private:
+    std::shared_ptr<vector<string>> check(size_t i, const string &msg) const {
+        auto ret = wptr.lock();
+        if (!ret) throw std::runtime_error("unbound StrBlobPtr");
+        if (i >= ret->size()) throw std::out_of_range(msg);
+        return ret;
+    }
+    std::weak_ptr<vector<string>> wptr;
+    size_t curr; 
+}; 
+
+class StrBlob {
+public:
+    using size_type = vector<string>::size_type;
+    friend class StrBlobPtr;
+    
+    StrBlobPtr begin() { return StrBlobPtr(*this); }
+    StrBlobPtr end() { return StrBlobPtr(*this, data->size()); }
+    
+    StrBlob():data(std::make_shared<vector<string>>()) { }
+    StrBlob(std::initializer_list<string> il):data(std::make_shared<vector<string>>(il)) { }
+    
+    size_type size() const { return data->size(); }
+    bool empty() const { return data->empty(); }
+    
+    void push_back(const string &t) { data->push_back(t); }
+    void pop_back() {
+        check(0, "pop_back on empty StrBlob");
+        data->pop_back();
+    }
+    const std::string& front() {
+        check(0, "front on empty StrBlob");
+        return data->front();
+    }
+    const std::string& back() {
+        check(0, "back on empty StrBlob");
+        return data->back();
+    }
+    
+private:
+    void check(size_type i, const string &msg) const {
+        if (i >= data->size()) throw std::out_of_range(msg);
+    }
+    
+private:
+    std::shared_ptr<vector<string>> data;
+};
