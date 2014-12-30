cb29608af88a685ead40c134ecaedd21123dd494
pezy-pc <urbancpz@gmail.com>
2014-12-30 17:13:33 +0800

    fixed header mistake

--- ch07/ex7_03.cpp
+++ ch07/ex7_03.cpp
@@ -1,12 +1,12 @@
 //
-//  ex7_3.cpp
-//  Exercise 7.3
+//  ex7_03.cpp
+//  Exercise 7.03
 //
 //  Created by pezy on 14/11/8.
 //  Copyright (c) 2014 pezy. All rights reserved.
 //
 
-#include "ex7_2.h"
+#include "ex7_02.h"
 #include <iostream>
 using std::cin; using std::cout; using std::endl;
 
