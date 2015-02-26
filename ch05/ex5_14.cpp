7c887128dc4ba13991ff844a9546f690fc30ca4b
Ocxs <ocxswork@gmail.com>
2015-02-26 23:44:42 +0800

    Update ex5_14.cpp
    it's a easy way to understand for primary learner

--- ch05/ex5_14.cpp
+++ ch05/ex5_14.cpp
@@ -19,3 +19,51 @@ int main()
     
     return 0;
 } 
+
+//it's a easy way to understand for primary learner
+/*
+#include<iostream>
+#include <vector>
+#include <string>
+using namespace std;
+ 
+int main()
+{
+	vector<string> svec;
+	string str;
+	while (cin >> str)
+	{
+		svec.push_back(str);
+	}
+	vector<string>::iterator it = svec.begin();//auto it = svec.begin();
+	int bigNum = 1, countNum = 1;
+	string bigStr, countStr;
+	while (it != svec.end()-1)
+	{
+		if (*it == *(it + 1))
+		{
+			++countNum;
+			countStr = *it;
+		}
+		else
+		{
+			if (countNum>bigNum)
+			{
+				bigStr = countStr;
+				bigNum = countNum;
+			}
+		}
+		++it;
+	}
+	if (bigNum==1)
+	{
+		cout << " there is no  duplicated string." << endl;
+	}
+	else
+	{
+		cout << "the word " << bigStr << " occurred  " << bigNum << "times." << endl;
+	}
+	system("pause");
+	return 0;
+}
+*/
