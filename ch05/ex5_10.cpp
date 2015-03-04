b163ecc1fd1631c310a845cfbea6f6e26826cdc1
pezy <urbancpz@gmail.com>
2015-03-04 19:17:45 +0800

    perfection for #19

--- ch05/ex5_10.cpp
+++ ch05/ex5_10.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using std::cin; using std::cout; using std::endl;
 
 int main()
@@ -7,58 +6,30 @@ int main()
     unsigned aCnt = 0, eCnt = 0, iCnt = 0, oCnt = 0, uCnt = 0;
     char ch;
     while (cin >> ch)
-        switch (ch)
+        switch (tolower(ch))
         {
             case 'a':
-            case 'A':
                 ++aCnt;
                 break;
             case 'e':
-            case 'E':
                 ++eCnt;
                 break;
             case 'i':
-            case 'I':
                 ++iCnt;
                 break;
             case 'o':
-            case 'O':
                 ++oCnt;
                 break;
             case 'u':
-            case 'U':
                 ++uCnt;
                 break;
         }
-//another way to solve it
-/*	while (cin >> ch)
-	{
-		ch = tolower(ch);
-		switch (ch)
-		{
-    		case 'a':
-    			++aCnt;
-    			break;
-    		case 'e':
-    			++eCnt;
-    			break;
-    		case 'i':
-    			++iCnt;
-    			break;
-    		case 'o':
-    			++oCnt;
-    			break;
-    		case 'u':
-    			++uCnt;
-    			break;
-		}
-	}
-*/    
+           
     cout << "Number of vowel a(A): \t" << aCnt << '\n'
-        << "Number of vowel e(E): \t" << eCnt << '\n'
-        << "Number of vowel i(I): \t" << iCnt << '\n'
-        << "Number of vowel o(O): \t" << oCnt << '\n'
-        << "Number of vowel u(U): \t" << uCnt << endl;
+         << "Number of vowel e(E): \t" << eCnt << '\n'
+         << "Number of vowel i(I): \t" << iCnt << '\n'
+         << "Number of vowel o(O): \t" << oCnt << '\n'
+         << "Number of vowel u(U): \t" << uCnt << endl;
     
     return 0;
 }
--- ch06/ex6_21.cpp
+++ ch06/ex6_21.cpp
@@ -6,24 +6,20 @@
 //! pointer points. What type should you use for the pointer?
 //!
 
-
-
 #include <iostream>
+using std::cout; 
 
-using namespace std;
-
-int LargerOne(const int _i,const int* _ip);
+int LargerOne(const int i, const int *ip)
+{
+    return (i > *ip) ? i : *ip;
+}
 
 int main()
 {
-    int c=6;
-    cout<<LargerOne(7,&c);
-
+    int c = 6;
+    cout << LargerOne(7, &c);
 
     return 0;
 }
 
-int LargerOne(const int _i, const int *_ip)
-{
-    return (_i > *_ip) ? _i : *_ip;
-}
+
