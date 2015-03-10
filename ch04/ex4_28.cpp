47c071c26a9e030446d489911f86cd412fa062b3
Jtaim <Jtaim@users.noreply.github.com>
2015-03-09 23:52:42 -0400

    Update ex4_28.cpp
    fixed the format issue.
    
    Keep up the good work. thanks

--- ch04/ex4_28.cpp
+++ ch04/ex4_28.cpp
@@ -10,36 +10,36 @@ int main()
     cout << "void: nullptr_t\t" << sizeof(nullptr_t) << " bytes" << endl << endl;
     
     // boolean type
-	cout << "bool:\t\t" << sizeof(bool) << " bytes" << endl << endl;
+    cout << "bool:\t\t" << sizeof(bool) << " bytes" << endl << endl;
     
     // charactor type
-	cout << "char:\t\t" << sizeof(char) << " bytes" << endl;
-	cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes" << endl;
-	cout << "char16_t:\t" << sizeof(char16_t) << " bytes" << endl;
-	cout << "char32_t:\t" << sizeof(char32_t) << " bytes" << endl << endl;
+    cout << "char:\t\t" << sizeof(char) << " bytes" << endl;
+    cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes" << endl;
+    cout << "char16_t:\t" << sizeof(char16_t) << " bytes" << endl;
+    cout << "char32_t:\t" << sizeof(char32_t) << " bytes" << endl << endl;
     
     // integers type
-	cout << "short:\t\t" << sizeof(short) << " bytes" << endl;
-	cout << "int:\t\t" << sizeof(int) << " bytes" << endl;
-	cout << "long:\t\t" << sizeof(long) << " bytes" << endl;
+    cout << "short:\t\t" << sizeof(short) << " bytes" << endl;
+    cout << "int:\t\t" << sizeof(int) << " bytes" << endl;
+    cout << "long:\t\t" << sizeof(long) << " bytes" << endl;
     cout << "long long:\t" << sizeof(long long) << " bytes" << endl << endl;
     
     // floating point type
-	cout << "float:\t\t" << sizeof(float) << " bytes" << endl;
-	cout << "double:\t\t" << sizeof(double) << " bytes" << endl;
-	cout << "long double:\t" << sizeof(long double) << " bytes" << endl << endl;
+    cout << "float:\t\t" << sizeof(float) << " bytes" << endl;
+    cout << "double:\t\t" << sizeof(double) << " bytes" << endl;
+    cout << "long double:\t" << sizeof(long double) << " bytes" << endl << endl;
 	
-    // Fixed width integers <cstdint> called in <iostream>
-	cout << "int8_t:\t\t" << sizeof(int8_t) << " bytes" << endl;
-	cout << "uint8_t:\t" << sizeof(uint8_t) << " bytes" << endl;
-	cout << "int16_t:\t" << sizeof(int16_t) << " bytes" << endl;
-	cout << "uint16_t:\t" << sizeof(uint16_t) << " bytes" << endl;
-	cout << "int32_t:\t" << sizeof(int32_t) << " bytes" << endl;
-	cout << "uint32_t:\t" << sizeof(uint32_t) << " bytes" << endl;	
-	cout << "int64_t:\t" << sizeof(int64_t) << " bytes" << endl;
-	cout << "uint64_t:\t" << sizeof(uint64_t) << " bytes" << endl;
-		
-	return 0;
+    // Fixed width integers
+    cout << "int8_t:\t\t" << sizeof(int8_t) << " bytes" << endl;
+    cout << "uint8_t:\t" << sizeof(uint8_t) << " bytes" << endl;
+    cout << "int16_t:\t" << sizeof(int16_t) << " bytes" << endl;
+    cout << "uint16_t:\t" << sizeof(uint16_t) << " bytes" << endl;
+    cout << "int32_t:\t" << sizeof(int32_t) << " bytes" << endl;
+    cout << "uint32_t:\t" << sizeof(uint32_t) << " bytes" << endl;
+    cout << "int64_t:\t" << sizeof(int64_t) << " bytes" << endl;
+    cout << "uint64_t:\t" << sizeof(uint64_t) << " bytes" << endl;
+    	
+    return 0;
 }
 
 //Print
