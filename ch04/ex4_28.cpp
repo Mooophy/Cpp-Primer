bd7107984dec8f57491cea37013ced7d96d7e276
Jtaim <Jtaim@users.noreply.github.com>
2015-03-09 22:36:50 -0400

    Update ex4_28.cpp
    would this be more complete?

--- ch04/ex4_28.cpp
+++ ch04/ex4_28.cpp
@@ -1,46 +1,72 @@
-/*
-    Name: Exercise 4.28
-    Copyright: 
-    Author: pezy
-    Date: 28/09/14 13:08
-    Description:  Write a program to print the size of each of the built-in types.
-*/
-
-
-#include <iostream>
-
-using std::cout; using std::endl;
+#include <iostream> // high level input/output operations.
 
 int main()
 {
-    cout << "bool\t\tis "      << sizeof(bool)        << "bytes." << endl;
-    cout << "char\t\tis "      << sizeof(char)        << "bytes." << endl;
-    cout << "wchar_t\t\tis "   << sizeof(wchar_t)     << "bytes." << endl;
-    cout << "char16_t\tis "    << sizeof(char16_t)    << "bytes." << endl;
-    cout << "char32_t\tis "    << sizeof(char32_t)    << "bytes." << endl;
-    cout << "short\t\tis "     << sizeof(short)       << "bytes." << endl;
-    cout << "int\t\tis "       << sizeof(int)         << "bytes." << endl;
-    cout << "long\t\tis "      << sizeof(long)        << "bytes." << endl;
-    cout << "long long\tis "   << sizeof(long long)   << "bytes." << endl;
-    cout << "float\t\tis "     << sizeof(float)       << "bytes." << endl;
-    cout << "double\t\tis "    << sizeof(double)      << "bytes." << endl;
-    cout << "long double\tis " << sizeof(long double) << "bytes." << endl;
-    cout << endl;
+    // by using method below only include what is needed.
+    using std::cout;
+    using std::endl;
     
-    return 0;
+    // void type
+    cout << "void: nullptr_t\t" << sizeof(nullptr_t) << " bytes" << endl << endl;
+    
+    // boolean type
+	cout << "bool:\t\t" << sizeof(bool) << " bytes" << endl << endl;
+    
+    // charactor type
+	cout << "char:\t\t" << sizeof(char) << " bytes" << endl;
+	cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes" << endl;
+	cout << "char16_t:\t" << sizeof(char16_t) << " bytes" << endl;
+	cout << "char32_t:\t" << sizeof(char32_t) << " bytes" << endl << endl;
+    
+    // integers type
+	cout << "short:\t\t" << sizeof(short) << " bytes" << endl;
+	cout << "int:\t\t" << sizeof(int) << " bytes" << endl;
+	cout << "long:\t\t" << sizeof(long) << " bytes" << endl;
+    cout << "long long:\t" << sizeof(long long) << " bytes" << endl << endl;
+    
+    // floating point type
+	cout << "float:\t\t" << sizeof(float) << " bytes" << endl;
+	cout << "double:\t\t" << sizeof(double) << " bytes" << endl;
+	cout << "long double:\t" << sizeof(long double) << " bytes" << endl << endl;
+	
+    // Fixed width integers <cstdint> called in <iostream>
+	cout << "int8_t:\t\t" << sizeof(int8_t) << " bytes" << endl;
+	cout << "uint8_t:\t" << sizeof(uint8_t) << " bytes" << endl;
+	cout << "int16_t:\t" << sizeof(int16_t) << " bytes" << endl;
+	cout << "uint16_t:\t" << sizeof(uint16_t) << " bytes" << endl;
+	cout << "int32_t:\t" << sizeof(int32_t) << " bytes" << endl;
+	cout << "uint32_t:\t" << sizeof(uint32_t) << " bytes" << endl;	
+	cout << "int64_t:\t" << sizeof(int64_t) << " bytes" << endl;
+	cout << "uint64_t:\t" << sizeof(uint64_t) << " bytes" << endl;
+		
+	return 0;
 }
 
 //Print
 /**************************/
-//bool            is 1bytes.
-//char            is 1bytes.
-//wchar_t         is 2bytes.
-//char16_t        is 2bytes.
-//char32_t        is 4bytes.
-//short           is 2bytes.
-//int             is 4bytes.
-//long            is 4bytes.
-//long long       is 8bytes.
-//float           is 4bytes.
-//double          is 8bytes.
-//long double     is 16bytes. 
+//void: nullptr_t 8 bytes
+
+//bool:           1 bytes
+
+//char:           1 bytes
+//wchar_t:        2 bytes
+//char16_t:       2 bytes
+//char32_t:       4 bytes
+
+//short:          2 bytes
+//int:            4 bytes
+//long:           4 bytes
+//long long:      8 bytes
+
+//float:          4 bytes
+//double:         8 bytes
+//long double:    16 bytes
+
+//int8_t:         1 bytes
+//uint8_t:        1 bytes
+//int16_t:        2 bytes
+//uint16_t:       2 bytes
+//int32_t:        4 bytes
+//uint32_t:       4 bytes
+//int64_t:        8 bytes
+//uint64_t:       8 bytes
