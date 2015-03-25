2c3c9b3b453706c680e1a0f3f1da0eb7d8eb306b
pezy <urbancpz@gmail.com>
2015-03-25 18:16:19 +0800

    fixed some bugs

--- ch11/ex11_3_4.cpp
+++ ch11/ex11_3_4.cpp
@@ -39,11 +39,10 @@ void ex11_3()
     std::map<std::string, std::size_t> word_count;
     std::string word;
     while(std::cin >> word)
-    {
         ++word_count[word];
-        for (const auto &elem : word_count)
-            std::cout << elem.first << " : " << elem.second <<"\n";
-    }
+
+    for (const auto &elem : word_count)
+        std::cout << elem.first << " : " << elem.second <<"\n";
 }
 
 int main()
