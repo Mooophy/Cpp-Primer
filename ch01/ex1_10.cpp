c330006a58df6ed5b62c09f0c6961a1084ee9dec
mudongliang <mudongliangabcd@gmail.com>
2015-04-03 18:55:34 +0800

    a little change about ++/-- and replace the some code with swap function!

--- ch01/ex1_10.cpp
+++ ch01/ex1_10.cpp
@@ -2,14 +2,10 @@
 
 #include <iostream>
 
-int main()
-{
+int main() {
 	int val = 10;
-	while (val >= 0)
-	{
-		std::cout << val << std::endl;
-		--val;
+	while (val >= 0){
+		std::cout << val-- << std::endl;
 	}
-	
 	return 0;
 }
