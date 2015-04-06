3b7dd627fe1355eda38e7c8e8e8af7846a8381b3
mudongliang <mudongliangabcd@gmail.com>
2015-04-06 22:48:30 +0800

    add ex2_28.cpp , because the errors showed by compiler is very userful to newbie!

---
+++ ch02/ex2_28.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: mudongliang
+	> Mail: mudongliangabcdi@163.com
+	> Created Time: Mon 06 Apr 2015 10:28:27 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(){ 
+	/*
+	int i, *const cp;       // illegal, cp must initialize.
+	int *p1, *const p2;     // illegal, p2 must initialize.
+	const int ic, &r = ic;  // illegal, ic must initialize.
+	const int *const p3;    // illegal, p3 must initialize.
+	const int *p;           // legal. a pointer to const int.
+	*/
+    return 0;
+}
