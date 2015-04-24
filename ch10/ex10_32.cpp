5fc880c9fde204bf5b31942bc5a47a4125f68734
pezy <urbancpz@gmail.com>
2015-04-24 09:06:59 +0800

    fixed ReadingLab/Discussion-for-Cpp#12

--- ch10/ex10_32.cpp
+++ ch10/ex10_32.cpp
@@ -24,7 +24,7 @@ int main()
     
     while (in_iter != in_eof)
         vec.push_back(*in_iter++);
-    sort(vec.begin(), vec.end(), compareIsbn);
+    sort(vec.begin(), vec.end(), [](Sales_item const& lhs, Sales_item const& rhs){ return lhs.isbn() < rhs.isbn(); });
     for (auto beg = vec.cbegin(), end = beg; beg != vec.cend(); beg = end) {
         end = find_if(beg, vec.cend(), [beg](const Sales_item &item){return item.isbn() != beg->isbn();});
         std::cout << std::accumulate(beg, end, Sales_item(beg->isbn())) << std::endl;
