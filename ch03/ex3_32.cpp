9c59857c5e8d38c1fa869962b123f1c05c840907
Yue Wang <Moophyalan@gmail.com>
2015-05-14 10:13:00 +1200

    polish

--- ch03/ex3_32.cpp
+++ ch03/ex3_32.cpp
@@ -5,26 +5,23 @@ using std::cout; using std::endl; using std::vector;
 
 int main()
 {
-    // use array
+    // array
     int ia[10];
-    for (auto i=0; i<10; ++i)
+    for (auto i = 0; i < 10; ++i)
         ia[i] = i;
 
     int ia2[10];
-    for (auto i=0; i<10; ++i)
+    for (auto i = 0; i < 10; ++i)
         ia2[i] = ia[i];
 
-    // use vector
-    size_t index=0;
+    // vector
+    size_t index = 0;
     vector<int> iv(10);
-    for(auto &i : iv)
-            i=index++;      // use post-increment operator to assign the value before it is incremented
+    for (auto &i : iv)
+        i = index++;      //post-increment operator
 
     vector<int> iv2(iv);
-
     for (auto i : iv2)
         cout << i << " ";
     cout << endl;
-
-    return 0;
 }
