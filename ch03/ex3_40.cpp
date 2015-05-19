d0cf41af2d4026367f91b26d586b9b3e30f649ac
pezy <urbancpz@gmail.com>
2015-05-19 09:34:45 +0800

    try to fixed #241

--- ch03/ex3_40.cpp
+++ ch03/ex3_40.cpp
@@ -1,23 +1,21 @@
+// Warning: Visual Studio 2013 cannot support C99 VLA(http://en.wikipedia.org/wiki/Variable-length_array)
+// please change compiler(such as gcc or clang) to run following codes.
+// more discuss: https://github.com/Mooophy/Cpp-Primer/pull/241
+// @pezy
+
 #include <iostream>
 #include <cstring>
 
-using std::cout; using std::endl;
-
 int main()
 {
-    const char* cs1 = "Wangyue";
-    const char* cs2 = "Pezy";
-    
-    size_t size = strlen(cs1) + strlen(cs2) + 1;
-    char* cs3 = new char(size);
-    
+    char cs1[] = "Hi";
+    char cs2[] = "frank67";
+
+    char cs3[strlen(cs1) + strlen(" ") + strlen(cs2) + 1];
+
     strcpy(cs3, cs1);
     strcat(cs3, " ");
     strcat(cs3, cs2);
-    
-    cout << cs3 << endl;
-    
-    delete cs3;
 
-    return 0;
+    std::cout << cs3 << std::endl;
 }
