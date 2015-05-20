2a59d1f44b25f86e10fffef6aa20e368bca58f2e
pezy <urbancpz@gmail.com>
2015-05-20 09:06:59 +0800

    improved formatting for ex3_40

--- ch03/ex3_40.cpp
+++ ch03/ex3_40.cpp
@@ -10,11 +10,11 @@ const char cstr2[]="world!";
 int main()
 {
     constexpr size_t new_size = strlen(cstr1) + strlen(" ") + strlen(cstr2) +1;
-	char cstr3[new_size];
-
-	strcpy(cstr3, cstr1);
-	strcat(cstr3, " ");
-	strcat(cstr3, cstr2);
-
-	std::cout << cstr3 << std::endl;
+    char cstr3[new_size];
+    
+    strcpy(cstr3, cstr1);
+    strcat(cstr3, " ");
+    strcat(cstr3, cstr2);
+    
+    std::cout << cstr3 << std::endl;
 }
