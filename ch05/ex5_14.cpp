b6194bc4511b08332a350b3526386c39ed2384ff
mudongliang <mudongliangabcd@gmail.com>
2015-05-26 19:07:09 +0800

    modify the indentation

--- ch05/ex5_14.cpp
+++ ch05/ex5_14.cpp
@@ -9,23 +9,23 @@ using std::string;
 int main()
 { 
     int count = 0,tmpcount = 0;
-	string currentstr,str;
+    string currentstr,str;
     for (string prestr; cin >> currentstr;) {
         if (currentstr == prestr) 
-			++tmpcount;
+            ++tmpcount;
         else {
-			if (count < tmpcount){
-				count = tmpcount;
-				str = prestr;
-			}
-			prestr = currentstr; //only currentstr != prestr , the assign is needed
-			tmpcount = 0;
-		}
+            if (count < tmpcount){
+                count = tmpcount;
+                str = prestr;
+            }
+            prestr = currentstr; //only currentstr != prestr , the assign is needed
+            tmpcount = 0;
+        }
     }
-	if (count < tmpcount){ // in case the last time that occurs tmpcount > count
-		count = tmpcount; 
-		str = currentstr;
-	}
+    if (count < tmpcount){ // in case the last time that occurs tmpcount > count
+        count = tmpcount; 
+        str = currentstr;
+    }
     if (count == 0) cout << "There's no duplicated string." << endl;
     else cout << "the word " << str << " occurred " << count + 1 << " times. " << endl;
     
