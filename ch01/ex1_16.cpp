66a3c84f96025e2796688b43303cb49c8603cbdc
Yue Wang <Moophyalan@gmail.com>
2015-06-19 11:08:54 +1200

    Update ex1_16.cpp

--- ch01/ex1_16.cpp
+++ ch01/ex1_16.cpp
@@ -2,15 +2,15 @@
 
 int main()
 {
-  int limit = 0, sum = 0, value = 0;
-  std::cout << "How many integers would you like to enter?";
-  std::cin >> limit;
+    int limit = 0, sum = 0, value = 0;
+    std::cout << "How many integers would you like to enter?";
+    std::cin >> limit;
 
-  // assume we don't know what is EOF(End-Of-File).
-  while (std::cin >> value && (--limit != 0))
-    sum += value;
+    // assume we don't know what is EOF(End-Of-File).
+    while (std::cin >> value && (--limit != 0))
+        sum += value;
 
-  std::cout << sum + value << std::endl;
+    std::cout << sum + value << std::endl;
 
-  return 0;
+    return 0;
 }
