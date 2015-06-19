3e6ca7f45994d7658e0e460d757e5b0f8bd9a898
Yue Wang <Moophyalan@gmail.com>
2015-06-19 15:02:55 +1200

    Update ex2_4.cpp

--- ch02/ex2_4.cpp
+++ ch02/ex2_4.cpp
@@ -2,14 +2,14 @@
 
 int main()
 {
-  unsigned u = 10, u2 = 42;
-  std::cout << u2 - u << std::endl; // 32
-  std::cout << u - u2 << std::endl; // 4294967264
-  int i = 10, i2 = 42;
-  std::cout << i2 - i << std::endl; // 32
-  std::cout << i - i2 << std::endl; // -32
-  std::cout << i - u << std::endl; // 0
-  std::cout << u - i << std::endl; // 0
-  
-  return 0;
+    unsigned u = 10, u2 = 42;
+    std::cout << u2 - u << std::endl;   // 32
+    std::cout << u - u2 << std::endl;   // 4294967264
+    int i = 10, i2 = 42;
+    std::cout << i2 - i << std::endl;   // 32
+    std::cout << i - i2 << std::endl;   // -32
+    std::cout << i - u << std::endl;    // 0
+    std::cout << u - i << std::endl;    // 0
+
+    return 0;
 }
