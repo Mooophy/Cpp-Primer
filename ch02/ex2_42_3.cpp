a1832e65e134306d9d65194304b6e44ff8a99b3d
Yue Wang <Moophyalan@gmail.com>
2015-06-19 15:05:17 +1200

    Update ex2_42_3.cpp

--- ch02/ex2_42_3.cpp
+++ ch02/ex2_42_3.cpp
@@ -3,36 +3,36 @@
 
 int main()
 {
-  Sales_data total;
-  double totalPrice;
-  if (std::cin >> total.bookNo >> total.units_sold >> totalPrice)
-  {
-    total.CalcRevenue(totalPrice);
-
-    Sales_data trans;
-    double transPrice;
-    while (std::cin >> trans.bookNo >> trans.units_sold >> transPrice)
+    Sales_data total;
+    double totalPrice;
+    if (std::cin >> total.bookNo >> total.units_sold >> totalPrice)
     {
-      trans.CalcRevenue(transPrice);
+        total.CalcRevenue(totalPrice);
 
-      if (total.bookNo == trans.bookNo)
-      {
-        total.AddData(trans);
-      }
-      else
-      {
+        Sales_data trans;
+        double transPrice;
+        while (std::cin >> trans.bookNo >> trans.units_sold >> transPrice)
+        {
+            trans.CalcRevenue(transPrice);
+
+            if (total.bookNo == trans.bookNo)
+            {
+                total.AddData(trans);
+            }
+            else
+            {
+                total.Print();
+                total.SetData(trans);
+            }
+        }
+
         total.Print();
-        total.SetData(trans);
-      }
+
+        return 0;
     }
-
-    total.Print();
-
-    return 0;
-  }
-  else
-  {
-    std::cerr << "No data?!" << std::endl;
-    return -1;  // indicate failure
-  }
+    else
+    {
+        std::cerr << "No data?!" << std::endl;
+        return -1;  // indicate failure
+    }
 }
