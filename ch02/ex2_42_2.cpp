9d5a3f40a8f0bbf0a0c218e85b05443d41bed73c
Yue Wang <Moophyalan@gmail.com>
2015-06-19 15:04:36 +1200

    Update ex2_42_2.cpp

--- ch02/ex2_42_2.cpp
+++ ch02/ex2_42_2.cpp
@@ -3,23 +3,22 @@
 
 int main()
 {
-  Sales_data book1, book2;
-  double price1, price2;
-  std::cin >> book1.bookNo >> book1.units_sold >> price1;
-  std::cin >> book2.bookNo >> book2.units_sold >> price2;
-  book1.CalcRevenue(price1);
-  book2.CalcRevenue(price2);
+    Sales_data book1, book2;
+    double price1, price2;
+    std::cin >> book1.bookNo >> book1.units_sold >> price1;
+    std::cin >> book2.bookNo >> book2.units_sold >> price2;
+    book1.CalcRevenue(price1);
+    book2.CalcRevenue(price2);
 
-  if (book1.bookNo == book2.bookNo)
-  {
-    book1.AddData(book2);
-    book1.Print();
-
-    return 0;
-  }
-  else
-  {
-    std::cerr << "Data must refer to same ISBN" << std::endl;
-    return -1;  // indicate failure
-  }
+    if (book1.bookNo == book2.bookNo)
+    {
+        book1.AddData(book2);
+        book1.Print();
+        return 0;
+    }
+    else
+    {
+        std::cerr << "Data must refer to same ISBN" << std::endl;
+        return -1;  // indicate failure
+    }
 }
