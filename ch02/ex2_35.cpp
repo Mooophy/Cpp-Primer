0d03d76fa1801383f427911da87a8e10dbf49993
Yue Wang <Moophyalan@gmail.com>
2015-06-19 15:02:07 +1200

    Update ex2_35.cpp

--- ch02/ex2_35.cpp
+++ ch02/ex2_35.cpp
@@ -3,19 +3,19 @@
 
 int main()
 {
-  const int i = 42;
-  auto j = i;
-  const auto &k = i;
-  auto *p = &i;
-  const auto j2 = i, &k2 = i;
+    const int i = 42;
+    auto j = i;
+    const auto &k = i;
+    auto *p = &i;
+    const auto j2 = i, &k2 = i;
 
-  // print i means int, and PKi means pointer to const int.
-  std::cout << "j is " << typeid(j).name()
-            << "\nk is " << typeid(k).name()
-            << "\np is " << typeid(p).name()
-            << "\nj2 is " << typeid(j2).name()
-            << "\nk2 is " << typeid(k2).name()
-            << std::endl;
+    // print i means int, and PKi means pointer to const int.
+    std::cout   << "j is "      << typeid(j).name()
+                << "\nk is "    << typeid(k).name()
+                << "\np is "    << typeid(p).name()
+                << "\nj2 is "   << typeid(j2).name()
+                << "\nk2 is "   << typeid(k2).name()
+                << std::endl;
 
-  return 0;
+    return 0;
 }
