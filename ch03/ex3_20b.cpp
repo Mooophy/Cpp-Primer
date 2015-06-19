69e42c972c0970ddf4f655945f55bc5d0a1e7a46
Yue Wang <Moophyalan@gmail.com>
2015-06-19 19:24:49 +1200

    Update ex3_20b.cpp

--- ch03/ex3_20b.cpp
+++ ch03/ex3_20b.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 #include <vector>
 
-using std::vector; 
-using std::cout; 
-using std::endl; 
+using std::vector;
+using std::cout;
+using std::endl;
 using std::cin;
 
 int main()
 {
-  vector<int> ivec;
-  int i;
-  while (cin >> i)
-    ivec.push_back(i);  
-  
-  if (ivec.empty())
-  {
-    cout << "input at least one integer." << endl;
-    return -1;
-  }
-  decltype(ivec.size()) size = ivec.size();
-  if (size%2 != 0) size = size/2 + 1;
-  else size /= 2;
+    vector<int> ivec;
+    for (int i; cin >> i; ivec.push_back(i));
 
-  for(decltype(size) i = 0; i != size; ++i)
-    cout << ivec[i] + ivec[ivec.size()-i-1] << " ";
-  cout << endl;  
+    if (ivec.empty())
+    {
+        cout << "input at least one integer." << endl;
+        return -1;
+    }
 
-  return 0;
+    auto size = ivec.size();
+    if (size % 2 != 0) size = size / 2 + 1;
+    else size /= 2;
+
+    for (int i = 0; i != size; ++i)
+        cout << ivec[i] + ivec[ivec.size() - i - 1] << " ";
+    cout << endl;
+
+    return 0;
 }
