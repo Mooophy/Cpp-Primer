aaaec6dbacc931e85ce982d063996db304130018
Yue Wang <Moophyalan@gmail.com>
2015-06-19 19:17:51 +1200

    Update ex3_1a.cpp

--- ch03/ex3_1a.cpp
+++ ch03/ex3_1a.cpp
@@ -8,13 +8,9 @@ using std::endl;
 
 int main()
 {
-  int sum = 0, val = 1;
-  // keep executing the while as long as val is less than or equal to 10
-  while (val <= 10)
-  {
-    sum += val;
-    ++val;
-  }
-  cout << "Sum of 1 to 10 inclusive is " << sum << endl;
-  return 0;
+    int sum = 0;
+    for (int val = 1; val <= 10; ++val) sum += val;
+    cout << "Sum of 1 to 10 inclusive is " << sum << endl;
+
+    return 0;
 }
