1499511a1e10b313476032de3e8898e0f25ad28a
Yue Wang <Moophyalan@gmail.com>
2015-06-20 18:15:24 +1200

    Update ex3_22.cpp

--- ch03/ex3_22.cpp
+++ ch03/ex3_22.cpp
@@ -8,19 +8,20 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <iterator>
+#include <cctype>
 
-using std::vector; using std::string; using std::cout; using std::cin; using std::endl;
+using std::vector; using std::string; using std::cout; using std::cin; using std::isalpha;
 
 int main()
 {
     vector<string> text;
     for (string line; getline(cin, line); text.push_back(line));
-
-    for (auto it = text.begin(); it != text.end() && !it->empty(); ++it) 
+    
+    for (auto& word : text)
     {
-        for (auto &c : *it) c = toupper(c);
-        cout << *it << endl;
+        for (auto& ch : word)
+            if (isalpha(ch)) ch = toupper(ch);
+        cout << word << " ";
     }
 
     return 0;
