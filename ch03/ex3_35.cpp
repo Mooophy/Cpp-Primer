8f554ce06e1b9aa574f0064082d7bfff2db6cefe
Yue Wang <Moophyalan@gmail.com>
2015-06-21 02:55:49 +1200

    Update ex3_35.cpp

--- ch03/ex3_35.cpp
+++ ch03/ex3_35.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
-#include <iterator>
-
-using std::begin; using std::end; using std::cout; using std::endl;
+using std::cout; using std::endl;
 
 int main()
 {
     int arr[10];
-    int *b = begin(arr);
-    int *e = end(arr);
-    
-    for (int *i = b; i != e; ++i)
-      *i = 0;
-      
-    for (auto i : arr)
-      cout << i << " ";
+    for (auto ptr = arr; ptr != arr + 10; ++ptr) *ptr = 0;
+
+    for (auto i : arr) cout << i << " ";
     cout << endl;
 
     return 0;
