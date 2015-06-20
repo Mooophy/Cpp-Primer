a1679963a7063555bcf92f04c8eeb8fcf59440b8
Yue Wang <Moophyalan@gmail.com>
2015-06-21 10:55:02 +1200

    Update ex3_36.cpp

--- ch03/ex3_36.cpp
+++ ch03/ex3_36.cpp
@@ -1,42 +1,31 @@
 #include <iostream>
 #include <vector>
-#include <iterator>
+using std::begin; using std::end; using std::cout; using std::endl; using std::vector; using std::size_t;
 
-using std::begin; using std::end; using std::cout; using std::endl; using std::vector;
-
-// pb point to begin of the array, pe point to end of the array.
-bool compare(int* const pb1, int* const pe1, int* const pb2, int* const pe2)
+bool compare(const int lhs[], size_t lsize, const int rhs[], size_t rsize)
 {
-  if ((pe1 - pb1) != (pe2 - pb2)) // have different size.
-    return false;
-  else
-  {
-    for (int* i = pb1, *j = pb2; (i != pe1)&&(j != pe2); ++i, ++j)
-      if (*i != *j) return false;
-  }
-  
-  return true;
+    if (lsize != rsize) return false;
+    for (int i = 0; i != lsize; ++i)
+        if (lhs[i] != rhs[i]) return false;
+    return true;
+}
+
+bool compare(vector<int> const& lhs, vector<int>& rhs)
+{
+    return lhs == rhs;
 }
 
 int main()
 {
-    int arr1[3] = {0, 1, 2};
-    int arr2[3] = {0, 2, 4};
-    
-    if (compare(begin(arr1), end(arr1), begin(arr2), end(arr2)))
-      cout << "The two arrays are equal." << endl;
-    else
-      cout << "The two arrays are not equal." << endl;
-      
-    cout << "==========" << endl;
-      
-    vector<int> vec1 = {0, 1, 2};
-    vector<int> vec2 = {0, 1, 2};
-    
-    if (vec1 == vec2)
-      cout << "The two vectors are equal." << endl;
-    else
-      cout << "The two vectors are not equal." << endl;
+    int arr1[3] = { 0, 1, 2 };
+    int arr2[3] = { 0, 2, 4 };
+    auto result = compare(arr1, sizeof(arr1) / sizeof(arr2[1]), arr2, sizeof(arr2[0]));
+    cout << "The two arrays are " << (result ? "equal\n" : "unequal\n");
+
+    vector<int> vec1 = { 0, 1, 2 };
+    vector<int> vec2 = { 0, 1, 2 };
+    result = compare(vec1, vec2);
+    cout << "The two vectors are " << (result ? "equal\n" : "unequal\n");
 
     return 0;
 }
