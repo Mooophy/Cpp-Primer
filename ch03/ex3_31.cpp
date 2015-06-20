ea86b12895fa6ab2e8dfde9aed2cde029e9a4c1f
Yue Wang <Moophyalan@gmail.com>
2015-06-21 02:48:02 +1200

    Update ex3_31.cpp

--- ch03/ex3_31.cpp
+++ ch03/ex3_31.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
-
 using std::cout; using std::endl;
 
 int main()
 {
-    int ia[10];
-    for (auto i = 0; i < 10; ++i)
-        ia[i] = i;
-
-    for (auto i : ia)
-        cout << i << " ";
+    int arr[10];
+    for (auto i = 0; i < 10; ++i) arr[i] = i;
+    for (auto i : arr) cout << i << " ";
     cout << endl;
-}
 
+    return 0;
+}
