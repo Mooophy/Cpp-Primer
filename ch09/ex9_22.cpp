b9a9eb36dd36ab563d7164495e7804657863b226
Yue Wang <Moophyalan@gmail.com>
2015-06-20 14:27:58 +1200

    Update ex9_22.cpp

--- ch09/ex9_22.cpp
+++ ch09/ex9_22.cpp
@@ -1,30 +1,19 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
-void insertDoubleValue(vector<int> &iv, int some_val)
+void double_and_insert(std::vector<int>& v, int some_val)
 {
-    vector<int>::iterator iter = iv.begin(), mid = iv.begin() + iv.size()/2;
-    while (iter != mid)
-        if (*mid == some_val)
-            mid = iv.insert(mid, 2 * some_val);
-        else --mid;
-}
-
-void print(const vector<int> &iv)
-{
-    for (auto i : iv)
-        cout << i << " ";
-    cout << endl;
+    auto mid = [&]{ return v.begin() + v.size() / 2; };
+    for (auto curr = v.begin(); curr != mid(); ++curr)
+        if (*curr == some_val)
+            ++(curr = v.insert(curr, 2 * some_val));
 }
 
 int main()
 {
-    vector<int> iv = {0,1,2,3,4,5,6,7,8,9};
-    iv.reserve(25);
-    cout << iv.capacity() << endl;
-    insertDoubleValue(iv, 4);
-    print(iv);
-}
+    std::vector<int> v{ 1, 9, 1, 9, 9, 9, 1, 1 };
+    double_and_insert(v, 1);
 
+    for (auto i : v) 
+        std::cout << i << std::endl;
+}
