9a8d5127abeb0f9eb7544021709790bbf4e615ab
Yue Wang <Moophyalan@gmail.com>
2015-06-20 18:21:18 +1200

    Update ex2_42_1.cpp

--- ch02/ex2_42_1.cpp
+++ ch02/ex2_42_1.cpp
@@ -3,11 +3,11 @@
 
 int main()
 {
-  Sales_data book;
-  double price;
-  std::cin >> book.bookNo >> book.units_sold >> price;
-  book.CalcRevenue(price);
-  book.Print();
+    Sales_data book;
+    double price;
+    std::cin >> book.bookNo >> book.units_sold >> price;
+    book.CalcRevenue(price);
+    book.Print();
 
-  return 0;
+    return 0;
 }
