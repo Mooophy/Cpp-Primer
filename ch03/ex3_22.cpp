977ad5ddec9dd005e6127d0c8880880f51aac11d
Yue Wang <Moophyalan@gmail.com>
2015-06-20 18:07:41 +1200

    Update ex3_22.cpp

--- ch03/ex3_22.cpp
+++ ch03/ex3_22.cpp
@@ -14,15 +14,14 @@ using std::vector; using std::string; using std::cout; using std::cin; using std
 
 int main()
 {
-  vector<string> text;
-  for (string line; getline(cin, line); )
-    text.push_back(line);
- 
-  for (auto it = text.begin(); it != text.end() && !it->empty(); ++it) {
-    for (auto &c : *it)
-      c = toupper(c);
-    cout << *it << endl;
-  }
+    vector<string> text;
+    for (string line; getline(cin, line); text.push_back(line));
 
-  return 0;
+    for (auto it = text.begin(); it != text.end() && !it->empty(); ++it) 
+    {
+        for (auto &c : *it) c = toupper(c);
+        cout << *it << endl;
+    }
+
+    return 0;
 }
