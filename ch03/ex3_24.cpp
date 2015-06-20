90a928bc99703f785837b824c9eb01828178ed4e
Yue Wang <Moophyalan@gmail.com>
2015-06-21 02:31:07 +1200

    Update ex3_24.cpp

--- ch03/ex3_24.cpp
+++ ch03/ex3_24.cpp
@@ -1,33 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <iterator>
 
 using std::vector; using std::cout; using std::endl; using std::cin;
 
 int main()
 {
-  int i;
-  vector<int> ivec;
-  while (cin >> i)
-    ivec.push_back(i);
+    vector<int> v;
+    for (int buffer; cin >> buffer; v.push_back(buffer));
 
-  if (ivec.empty())
-  {
-    cout << "input at least one integer." << endl;
-    return -1;
-  }
-  else if(ivec.size() == 1)
-  {
-    cout << *ivec.begin() << " has no adjacent elements.";
-  }
-  
-  for (auto it = ivec.begin(); it+1 != ivec.end(); ++it)
-    cout << *it + *(it+1) << " ";
-  cout << endl;
+    if (v.size() < 2)
+    {
+        cout << " please enter at least two integers";
+        return -1;
+    }
 
-  for (auto beg = ivec.begin(), end = ivec.end()-1; beg <= end; ++beg, --end)
-    cout << *beg + *end << " ";
-  cout << endl;
+    for (auto it = v.cbegin(); it + 1 != v.cend(); ++it)
+        cout << *it + *(it + 1) << " ";
+    cout << endl;
 
-  return 0;
+    for (auto lft = v.cbegin(), rht = v.cend() - 1; lft <= rht; ++lft, --rht)
+        cout << *lft + *rht << " ";
+    cout << endl;
+
+    return 0;
 }
