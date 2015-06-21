2eed0aab61d89511bc9c8a468854f3417ae22c47
Yue Wang <Moophyalan@gmail.com>
2015-06-21 13:57:18 +1200

    Update ex3_4b.cpp

--- ch03/ex3_4b.cpp
+++ ch03/ex3_4b.cpp
@@ -10,14 +10,13 @@ using std::endl;
 
 int main()
 {
-  string str1, str2;
-  while (cin >> str1 >> str2)
-  {
-    if (str1.size() == str2.size())
-      cout << "The two strings have the same length." << endl;
-    else
-      cout << "The longer string is " << ((str1.size() > str2.size()) ? str1 : str2);
-  }
+    for (string str1, str2; cin >> str1 >> str2;/* */)
+    {
+        if (str1.size() == str2.size())
+            cout << "The two strings have the same length." << endl;
+        else
+            cout << "The longer string is " << ((str1.size() > str2.size()) ? str1 : str2) << endl;
+    }
 
-  return 0;
+    return 0;
 }
