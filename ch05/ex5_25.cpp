bf468ce3784f90558df43cb08a9162a9ce35fbce
Yue Wang <Moophyalan@gmail.com>
2015-06-22 17:39:51 +1200

    Update ex5_25.cpp

--- ch05/ex5_25.cpp
+++ ch05/ex5_25.cpp
@@ -4,19 +4,19 @@ using std::cin; using std::cout; using std::endl; using std::runtime_error;
 
 int main(void)
 {
-	int a, b;
-	cout << "Input two integers: ";
-	while (cin >> a >> b)
-	{
-	    try {
-	        if (b == 0) throw runtime_error("divisor is 0");
-	        cout << static_cast<double>(a) / b << endl;
-	        cout << "Input two integers: ";
-	    } catch (runtime_error err) {
-	        cout << err.what() << "\n"
-	             << "Try again.\nInput two integers: ";
-	    }
-	}
+    for (int i, j; cout << "Input two integers:\n", cin >> i >> j; )
+    {
+        try 
+        {
+            if (j == 0) 
+                throw runtime_error("divisor is 0");
+            cout << i / j << endl;
+        }
+        catch (runtime_error err) 
+        {
+            cout << err.what() << "\n";
+        }
+    }
 
-	return 0;
+    return 0;
 }
