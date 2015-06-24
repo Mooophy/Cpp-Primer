296fd5176fb63f3e23e629e3dfdcb82353ac093e
Yue Wang <Moophyalan@gmail.com>
2015-06-24 18:50:56 +1200

    Update ex9_50.cpp

--- ch09/ex9_50.cpp
+++ ch09/ex9_50.cpp
@@ -1,4 +1,4 @@
-//! @Alan
+//! @Yue Wang
 //!
 //! Exercise 9.50:
 //! Write a program to process a vector<string>s whose elements represent integral values.
@@ -10,37 +10,27 @@
 #include <string>
 #include <vector>
 
+int sum_for_int(const std::vector<std::string> const& v)
+{
+    int sum = 0;
+    for (auto const& s : v)
+        sum += std::stoi(s);
+    return sum;
+}
 
-int sum(const std::vector<std::string> &v);
-float sum_f(const std::vector<std::string> &v);
+float sum_for_float(const std::vector<std::string> const& v)
+{
+    float sum = 0.0;
+    for (auto const& s : v)
+        sum += std::stof(s);
+    return sum;
+}
 
 int main()
 {
-    std::vector<std::string> v = {"1","2","3","4.5"};
-    std::cout << sum(v)<<"\n";
-    std::cout << sum_f(v);
+    std::vector<std::string> v = { "1", "2", "3", "4.5" };
+    std::cout << sum_for_int(v) << std::endl;
+    std::cout << sum_for_float(v) << std::endl;
+
     return 0;
-}
-
-
-int sum(const std::vector<std::string> &v)
-{
-    int sum=0;
-    for(auto &s : v)
-    {
-        sum += std::stoi(s);
-    }
-
-    return sum;
-}
-
-
-float sum_f(const std::vector<std::string> &v)
-{
-    float sum = 0.0;
-    for(auto &s : v)
-    {
-        sum += std::stof(s);
-    }
-    return sum;
 }
