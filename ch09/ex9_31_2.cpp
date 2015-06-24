0d4a21288ff41df744dec487f9486cb39d8ecdec
Yue Wang <Moophyalan@gmail.com>
2015-06-24 18:37:08 +1200

    Update ex9_31_2.cpp

--- ch09/ex9_31_2.cpp
+++ ch09/ex9_31_2.cpp
@@ -20,21 +20,20 @@ using std::forward_list; using std::cout; using std::endl; using std::advance;
 
 int main()
 {
-    forward_list<int> vi = {0,1,2,3,4,5,6,7,8,9}; 
+    forward_list<int> vi = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     auto iter = vi.begin(), prev = vi.before_begin();
-    while (iter != vi.end()) {    
-        if (*iter % 2) {        
-            iter = vi.insert_after(prev, *iter);   
+    while (iter != vi.end()) {
+        if (*iter % 2) {
+            iter = vi.insert_after(prev, *iter);
             advance(iter, 2);
             advance(prev, 2);
-        } else        
+        }
+        else
             iter = vi.erase_after(prev);
     }
-    
+
     for (auto i : vi)
         cout << i << " ";
-    
+
     return 0;
 }
-
-
