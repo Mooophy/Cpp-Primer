7636233d26b459a8002b2d05e1ad570e3a99a7ec
Yue Wang <Moophyalan@gmail.com>
2015-06-24 16:21:58 +1200

    Update ex9_27.cpp

--- ch09/ex9_27.cpp
+++ ch09/ex9_27.cpp
@@ -7,23 +7,20 @@
 //
 //  @Brief  Write a program to find and remove the odd-valued
 //          elements in a forward_list<int>.
+//
+//  @Refactored by @Yue Wang Jun 2015
+//
 
 #include <iostream>
 #include <forward_list>
 
-using std::forward_list; using std::cout; using std::endl;
+using std::forward_list; using std::cout;
 
 int main()
 {
-    forward_list<int> flst = {0,1,2,3,4,5,6,7,8,9};
-    for (auto prev = flst.before_begin(), curr = flst.begin(); curr != flst.end(); )
-        if (*curr & 0x1) curr = flst.erase_after(prev);
-        else prev = curr++;
-    
-    for (auto i : flst)
-        cout << i << " ";
-    
+    forward_list<int> li = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    li.remove_if([](int i){ return i & 0x1; });
+    for (auto i : li) cout << i << " ";
+
     return 0;
 }
-
-
