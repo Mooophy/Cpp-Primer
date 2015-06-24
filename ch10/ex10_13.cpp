4ecd2af654f45c6564d1474b5a97ba44ef5f200c
Yue Wang <Moophyalan@gmail.com>
2015-06-24 19:41:12 +1200

    Update ex10_13.cpp

--- ch10/ex10_13.cpp
+++ ch10/ex10_13.cpp
@@ -25,6 +25,7 @@ int main()
 {
     auto v = std::vector<std::string>{ "a", "as", "aasss", "aaaaassaa", "aaaaaabba", "aaa" };
     auto pivot = std::partition(v.begin(), v.end(), predicate);
+    
     for (auto it = v.cbegin(); it != pivot; ++it)
         std::cout << *it << " ";
     std::cout << std::endl;
