de2bb055657c0c93bd7a55ab8f481da0fba0c910
Yue Wang <Moophyalan@gmail.com>
2015-06-28 22:27:36 +1200

    Update ex14_26_StrBlobTest.cpp

--- ch14/ex14_26_StrBlobTest.cpp
+++ ch14/ex14_26_StrBlobTest.cpp
@@ -3,16 +3,16 @@
 
 int main()
 {
-	StrBlob sb1{ "a", "b", "c" };
+    StrBlob sb1{ "a", "b", "c" };
     StrBlob sb2 = sb1;
 
     sb2[2] = "b";
 
-	if (sb1 > sb2) {
-		for (ConstStrBlobPtr iter = sb1.cbegin(); iter < sb1.cend(); iter.incr())
-			std::cout << iter.deref() << " ";
-		std::cout << std::endl;
-	}
+    if (sb1 > sb2) {
+        for (ConstStrBlobPtr iter = sb1.cbegin(); iter < sb1.cend(); iter.incr())
+            std::cout << iter.deref() << " ";
+        std::cout << std::endl;
+    }
 
     StrBlobPtr iter(sb2);
     std::cout << iter[2] << std::endl;
