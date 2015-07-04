4db449633925183098233362d1fa889db5bf18c7
Isaac Ge <acgtyrant@gmail.com>
2015-07-04 19:13:55 +0800

    ex9_18.cpp: use range for statement
    a better feature!

--- ch09/ex9_18.cpp
+++ ch09/ex9_18.cpp
@@ -18,8 +18,7 @@ int main()
 {
     deque<string> input;
     for (string str; cin >> str; input.push_back(str));
-    for (auto iter = input.cbegin(); iter != input.cend(); ++iter)
-        cout << *iter << endl;
+    for (auto &iter : input) cout << iter << endl;
 
     return 0;
 }
