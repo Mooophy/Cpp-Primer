9f71786a042f943b7c45fded1c2003922bb8bdd2
Yue Wang <Moophyalan@gmail.com>
2015-07-07 03:52:30 +1200

    Update ex11_24_25_26.cpp

--- ch11/ex11_24_25_26.cpp
+++ ch11/ex11_24_25_26.cpp
@@ -26,12 +26,12 @@
 
 int main()
 {
-	//! ex11.26
-	std::map<int, std::string> m = { { 1,"ss" },{ 2,"sz" } };
-	using KeyType = std::map<int, std::string>::key_type;
+    //! ex11.26
+    std::map<int, std::string> m = { { 1,"ss" },{ 2,"sz" } };
+    using KeyType = std::map<int, std::string>::key_type;
 
-	std::cout << "type to subscript: " << typeid(KeyType).name() << std::endl;
-	std::cout << "returned from the subscript operator: " << typeid(decltype(m[1])).name() << std::endl;
+    std::cout << "type to subscript: " << typeid(KeyType).name() << std::endl;
+    std::cout << "returned from the subscript operator: " << typeid(decltype(m[1])).name() << std::endl;
 
-	return 0;
+    return 0;
 }
