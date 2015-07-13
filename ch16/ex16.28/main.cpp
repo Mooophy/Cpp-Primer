90d26e8e9367f29397e510e0e15468453a313572
Yue Wang <Moophyalan@gmail.com>
2015-07-14 00:19:35 +1200

    Update main.cpp

--- ch16/ex16.28/main.cpp
+++ ch16/ex16.28/main.cpp
@@ -1,36 +1,29 @@
 /***************************************************************************
- *  @file       main.cpp
- *  @author     Alan.W
- *  @date       04  Feb 2014
- *  @remark     This code is for the exercises from C++ Primer 5th Edition
- *  @note
- ***************************************************************************/
+*  @file       main.cpp
+*  @author     Yue Wang
+*  @date       04  Feb 2014
+*                  Jul 2015  
+*  @remark     This code is for the exercises from C++ Primer 5th Edition
+*  @note
+***************************************************************************/
 //!
 //! Exercise 16.28 Write your own versions of shared_ptr and unique_ptr
 //!
 
-
 #include <iostream>
-#include <vector>
-#include <memory>
-#include <functional>
-#include "DebugDelete.h"
-#include "shared_pointer.h"
-#include "unique_pointer.h"
-
-
-
+#include <string>
+#include "shared_pointer.hpp"
 
 int main()
 {
-    std::vector<unique_pointer<int>> v;
+    auto sp = cp5::SharedPointer<int>{ new int(42) };
+    auto sp2 = sp;
+    std::cout << *sp << std::endl;
+    std::cout << sp.use_count() << std::endl;
+    
+    auto string_ptr = cp5::SharedPointer<std::string>{ new std::string{ "Yue" } };
+    std::cout << *string_ptr << std::endl;
+    std::cout << string_ptr->size() << std::endl;
 
-    for(int u = 0; u !=10 ; ++u)
-        v.push_back(unique_pointer<int>(new int(u)));
-
-    for(auto& sp : v)
-        std::cout << *sp << "\n";
-
-
+    return 0;
 }
-
