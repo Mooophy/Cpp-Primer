7ef6d2d00bf66ab28c5dee2db49363444c9b8a9c
yue <MoophyAlan@gmail.com>
2015-08-21 11:18:45 +1200

    refactored

--- ch16/ex16.63.64/main.cpp
+++ ch16/ex16.63.64/main.cpp
@@ -1,7 +1,8 @@
 /***************************************************************************
  *  @file       main.cpp
- *  @author     Alan.W
+ *  @author     Yue Wang
  *  @date       3  Mar 2014
+ *              Aug, 2015
  *  @remark     This code is for the exercises from C++ Primer 5th Edition
  *  @note
  ***************************************************************************/
@@ -23,47 +24,32 @@
 
 //! template
 template<typename T>
-std::size_t count(const std::vector<T>& vec, T value);
+std::size_t  count(std::vector<T> const& vec, T value) 
+{
+    auto count = 0u;
+    for(auto const& elem  : vec)
+        if(value == elem) ++count;
+    return count;
+}
 
 //! template specialization
 template<>
-std::size_t count (const std::vector<const char*> &vec, const char* value);
-
+std::size_t count (std::vector<const char*> const& vec, const char* value)
+{
+    auto count = 0u;
+    for(auto const& elem : vec)
+        if(0 == strcmp(value, elem)) ++count;
+    return count;
+}
 int main()
 {
     //! for ex16.63
-/*
-    std::vector<int> vi = {1,2,3,4,5,6,7,1,1,1,1};
-    std::vector<double> vd = {1.1,1.1,2.3,4};
-
-    std::cout << count(vd, 1.1);
-*/
+    std::vector<double> vd = { 1.1, 1.1, 2.3, 4 };
+    std::cout << count(vd, 1.1) << std::endl;
+    
     //! for ex16.64
-    std::vector<const char*> vcc = {"alan","alan","alan","alan","moophy"};
-    std::cout << count(vcc, "alan");
+    std::vector<const char*> vcc = { "alan", "alan", "alan", "alan", "moophy" };
+    std::cout << count(vcc, "alan") << std::endl;
 
     return 0;
-}
-
-//! template
-template<typename T>
-std::size_t count(const std::vector<T>& vec, T value)
-{
-    std::size_t count = 0;
-    for(auto& item : vec)
-        count += (item == value)? 1 : 0 ;
-
-    return count;
-}
-
-//! template specialization
-//! where T == const char*
-template<>
-std::size_t count (const std::vector<const char*> &vec, const char* value)
-{
-    std::size_t count = 0;
-    for(auto& item : vec)
-        count += (strcmp(item,value) == 0)? 1 : 0;
-
-    return count;
 }
