1338464fef0cccada63713c57008209e0bc1a3e6
yue <MoophyAlan@gmail.com>
2015-08-21 11:48:48 +1200

    polished

--- ch16/ex16.53.54.55/main.cpp
+++ ch16/ex16.53.54.55/main.cpp
@@ -26,15 +26,15 @@
 
 // trivial case 
 template<typename Printable>
-std::ostream& print(std::ostream& os, Printable const& t)
+std::ostream& print(std::ostream& os, Printable const& printable)
 {
-    return os << t;
+    return os << printable;
 }
 // recursion
 template<typename Printable, typename... Args>
-std::ostream& print(std::ostream& os, Printable const& t, Args const&... rest)
+std::ostream& print(std::ostream& os, Printable const& printable, Args const&... rest)
 {
-    return print(os << t << ", ", rest...);
+    return print(os << printable << ", ", rest...);
 }
 
 int main()
