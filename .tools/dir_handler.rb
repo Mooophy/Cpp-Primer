af6f34e5c59a04b1fe7834a335f061f6fb0cc8c7
Mooophy <moophyalan@gmail.com>
2015-08-27 15:11:49 +1200

    new file:   dir_handler.rb 	new file:   test.txt

---
+++ .tools/dir_handler.rb
@@ -0,0 +1,24 @@
+class DirHandler
+  attr_reader :paths
+
+  def initialize dir, extension
+    @paths = list_paths_recursively dir, extension
+  end
+
+  def on_each_line 
+    @paths.each do |path|
+      content = File.readlines path
+      content.each { |line| yield line }
+      write_back path, content 
+    end
+  end  
+
+private
+  def list_paths_recursively dir, extension
+    Dir.glob "#{ dir }/**/*.#{ extension }"
+  end
+
+  def write_back filename, content
+    File.open(filename, 'w+'){ |file| file.puts content }
+  end
+end
