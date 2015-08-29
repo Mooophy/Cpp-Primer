27883863c68b77defcca107a92baa3266f905b27
Mooophy <moophyalan@gmail.com>
2015-08-30 11:29:57 +1200

    refactor 	modified:   format.rb

--- .tools/format.rb
+++ .tools/format.rb
@@ -1,21 +1,50 @@
 require_relative 'dir_handler'
 
-extensions = [ "cpp", "h", "hpp", "md" ]
-extensions.freeze
-dir = ARGV[0] 
-dir.freeze
+#extensions = [ "cpp", "h", "hpp", "md" ]
+#extensions.freeze
+#dir = ARGV[0] 
+#dir.freeze
+#
+##init handlers for all types of files
+#handlers = extensions.collect{ |e| DirHandler.new dir, e }
+#
+##for comma:
+##   1,2,3,4   =>  1, 2, 3, 4
+#handlers.each do |h|
+#  h.on_each_line do |line| 
+#    begin
+#      line.gsub! /,(\S)/, ', \1' unless line.match /.*\".*,.*\".*/ or line.match /','/
+#    rescue Exception => e
+#      puts e.message + ", ignored."
+#    end
+#  end
+#end
+#
 
-#init handlers for all types of files
-handlers = extensions.collect{ |e| DirHandler.new dir, e }
+class Format
+  attr_reader :extensions
 
-#for comma:
-#   1,2,3,4   =>  1, 2, 3, 4
-handlers.each do |h|
-  h.on_each_line do |line| 
-    begin
-      line.gsub! /,(\S)/, ', \1' unless line.match /.*\".*,.*\".*/ or line.match /','/
-    rescue Exception => e
-      puts e.message + ", ignored."
-    end
+  def initialize dir, extensions
+    @dir = dir
+    @extensions = extensions
+    @dir.freeze
+    @extensions.freeze
+    @handlers = extensions.collect{ |e| DirHandler.new dir, e }
   end
-end
+
+  def for_commas
+    @handlers.each do |h|
+      h.on_each_line do |line| 
+        begin
+          line.gsub! /,(\S)/, ', \1' unless line.match /.*\".*,.*\".*/ or line.match /','/
+        rescue Exception => e
+          puts e.message + ", ignored."
+        end
+      end
+    end
+  end 
+end    
+
+format = Format.new ARGV[0], [ "cpp", "h", "hpp", "md" ]
+format.for_commas
+
