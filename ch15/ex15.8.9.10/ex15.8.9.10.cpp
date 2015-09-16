165bbf2a776cec28574901d270e38af8bd65dc4c
XDXX <h243529720@gmail.com>
2015-09-16 15:39:18 +0800

    fixed a little typo in ex15.8.9.10.cpp.

--- ch15/ex15.8.9.10/ex15.8.9.10.cpp
+++ ch15/ex15.8.9.10/ex15.8.9.10.cpp
@@ -10,7 +10,7 @@
 // Define static type and dynamic type.
 //  The static type of an expression is always known at compile time.
 //  The dynamic type is the type of the object in memory that the variable or
-//  expression reprensents. The dynamic type may not be known until run time.
+//  expression represents. The dynamic type may not be known until run time.
 //
 // Exercise 15.9:
 // When is it possible for an expression’s static type to differ from its
