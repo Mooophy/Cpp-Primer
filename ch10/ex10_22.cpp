c2c122bcbd0da31ef0248ac76d1df486f2e1ef0a
pezy <urbancpz@gmail.com>
2015-10-12 19:02:58 +0800

    Improved #344

--- ch10/ex10_22.cpp
+++ ch10/ex10_22.cpp
@@ -3,6 +3,7 @@
 //  Exercise 10.22
 //
 //  Created by pezy on 12/11/14.
+//	Updated by pezy on 10/12/15.
 //
 //  @Brief  Rewrite the program to count words of size 6 or less using
 //          functions in place of the lambdas.
@@ -16,13 +17,13 @@
 using std::string;
 using namespace std::placeholders;
 
-bool isBiggerThan6(const string &s, string::size_type sz)
+bool isLesserThanOrEqualTo6(const string &s, string::size_type sz)
 {
-    return s.size() <= sz;
+	return s.size() <= sz;
 }
 
 int main()
 {
-    std::vector<string> authors{ "Mooophy", "pezy", "Queequeg90", "shbling", "evan617" };
-    std::cout << count_if(authors.cbegin(), authors.cend(), bind(isBiggerThan6, _1, 6));
+	std::vector<string> authors{ "Mooophy", "pezy", "Queequeg90", "shbling", "evan617" };
+	std::cout << count_if(authors.cbegin(), authors.cend(), bind(isLesserThanOrEqualTo6, _1, 6));
 }
