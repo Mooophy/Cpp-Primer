9f44b243747c8bb92059295382be9f2bc37441b9
Yue Wang <Moophyalan@gmail.com>
2015-10-22 11:29:07 +1300

    Update ex9_43_1.cpp

--- ch09/ex9_43_1.cpp
+++ ch09/ex9_43_1.cpp
@@ -7,37 +7,41 @@
 //  @Brief  Write a function that takes three strings, s, oldVal, and newVal.
 //          Using iterators, and the insert and erase functions replace
 //          all instances of oldVal that appear in s by newVal.
-//	    Test your function by using it to replace common abbreviations,
+//	        Test your function by using it to replace common abbreviations,
 //          such as “tho” by “though” and “thru” by “through”.
-//  @notice This program doesn't compile on GCC because GCC(4.9.2) doesn't 
-//	    support the feature of c++11 which the
-//          insert(iterator p, InputIterator first, InputIterator last) function
-//	    could return an iterator.So please use VS(2012+) or clang instead.
-//	    Or you can see the cross-platform version(ex9_43_2.cpp) of this exercise.
+//
+//  @note   This code doesn't compile on GCC. Please use Visual Studio 2015+ or Clang 3.6+ 
+//
+//  
+//  Refactored by Yue Wang Oct, 2015 
+//
 
 #include <iterator>
 #include <iostream>
 #include <string>
 #include <cstddef>
 
-using std::cout; using std::endl; using std::string;
+using std::cout; 
+using std::endl; 
+using std::string;
 
-void func(string &s, string const& oldVal, string const& newVal)
+auto replace_with(string &s, string const& oldVal, string const& newVal)
 {
-    for (auto iter = s.begin(); std::distance(iter, s.end()) >= static_cast<ptrdiff_t>(oldVal.size()); )
-        if (*iter == oldVal[0] && string(iter, std::next(iter, oldVal.size())) == oldVal) {
-            iter = s.erase(iter, iter + oldVal.size());
-            iter = s.insert(iter, newVal.begin(), newVal.end());
-            iter += newVal.size();
-        } else  ++iter;
+    for (auto cur = s.begin(); cur <= s.end() - oldVal.size(); )
+        if (oldVal == string{ cur, cur + oldVal.size() })
+            cur = s.erase(cur, cur + oldVal.size()),
+            cur = s.insert(cur, newVal.begin(), newVal.end()),
+            cur += newVal.size();
+        else  
+            ++cur;
 }
 
-int main() 
+int main()
 {
-	string s{ "To drive straight thru is a foolish, tho courageous act." };
-	func(s, "tho", "though");
-	func(s, "thru", "through");
-	cout << s << endl;
+    string s{ "To drive straight thru is a foolish, tho courageous act." };
+    replace_with(s, "tho", "though");
+    replace_with(s, "thru", "through");
+    cout << s << endl;
 
-	return 0;
+    return 0;
 }
