0b62871b1b40c2815a496fe63bf05dbb42b8b8cf
Yue Wang <Moophyalan@gmail.com>
2015-10-21 19:30:30 +1300

    Update ex9_34.cpp

--- ch09/ex9_34.cpp
+++ ch09/ex9_34.cpp
@@ -14,29 +14,29 @@
 //					iter = vi.insert(iter, *iter);    
 //				++iter; 
 //
-//  @Answer "infinite loop". Casue the `++iter` out of the `while` loop. But even through add brackets it is still infinite loop.
-//          Fixed following.
+//  @Answer "infinite loop". Becasue the `++iter` is outside of the scope of the `while` loop.
+//          Fixed as following.
+//
+//  Refactored by Yue Wang Oct, 2015
+//
+
 
 #include <iostream>
-using std::cout; using std::endl;
-
 #include <vector>
+
+using std::cout;
+using std::endl;
 using std::vector;
 
 int main()
 {
-	vector<int> vi = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-	auto iter = vi.begin();
-	while (iter != vi.end()) {
-		if (*iter % 2) {
-			iter = vi.insert(iter, *iter);
-			++iter;
-		}
-		++iter;
-	}
+    vector<int> data { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    for(auto cur = data.begin(); cur != data.end(); ++cur)
+        if(*cur & 0x1)
+            cur = data.insert(cur, *cur), ++cur;
+    
+    for (auto i : data)
+        cout << i << " ";
 
-	for (auto i : vi)
-		cout << i << " ";
-
-	return 0;
+    return 0;
 }
