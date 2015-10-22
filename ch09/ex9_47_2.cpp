1977eab13cb327faab5320c8d966df68b404775f
Yue Wang <Moophyalan@gmail.com>
2015-10-22 13:10:37 +1300

    Update ex9_47_2.cpp

--- ch09/ex9_47_2.cpp
+++ ch09/ex9_47_2.cpp
@@ -20,14 +20,14 @@ int main()
     string numbers{ "123456789" };
     string alphabet{ "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" };
     string str{ "ab2c3d7R4E6" };
-    
+
     cout << "numeric characters: ";
-    for (string::size_type pos = 0; (pos = str.find_first_not_of(alphabet, pos)) != string::npos; ++pos)
+    for (int pos = 0; (pos = str.find_first_not_of(alphabet, pos)) != string::npos; ++pos)
         cout << str[pos] << " ";
     cout << "\nalphabetic characters: ";
-    for (string::size_type pos = 0; (pos = str.find_first_not_of(numbers, pos)) != string::npos; ++pos)
+    for (int pos = 0; (pos = str.find_first_not_of(numbers, pos)) != string::npos; ++pos)
         cout << str[pos] << " ";
     cout << endl;
-    
+
     return 0;
 }
