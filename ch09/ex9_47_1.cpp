12a80c5dd1a19ee4cc425213b007556f409c7901
Yue Wang <Moophyalan@gmail.com>
2015-10-22 13:09:06 +1300

    Update ex9_47_1.cpp

--- ch09/ex9_47_1.cpp
+++ ch09/ex9_47_1.cpp
@@ -13,21 +13,23 @@
 #include <string>
 #include <iostream>
 
-using std::string; using std::cout; using std::endl;
+using std::string;
+using std::cout;
+using std::endl;
 
 int main()
 {
     string numbers{ "123456789" };
     string alphabet{ "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" };
     string str{ "ab2c3d7R4E6" };
-    
+
     cout << "numeric characters: ";
-    for (string::size_type pos = 0; (pos = str.find_first_of(numbers, pos)) != string::npos; ++pos)
+    for (int pos = 0; (pos = str.find_first_of(numbers, pos)) != string::npos; ++pos)
         cout << str[pos] << " ";
     cout << "\nalphabetic characters: ";
-    for (string::size_type pos = 0; (pos = str.find_first_of(alphabet, pos)) != string::npos; ++pos)
+    for (int pos = 0; (pos = str.find_first_of(alphabet, pos)) != string::npos; ++pos)
         cout << str[pos] << " ";
     cout << endl;
-    
+
     return 0;
 }
