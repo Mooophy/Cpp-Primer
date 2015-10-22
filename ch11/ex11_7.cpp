7d13c2ec07adeca2cddcbf461c8de5e3391aa108
Yue Wang <Moophyalan@gmail.com>
2015-10-23 09:43:25 +1300

    Update ex11_7.cpp

--- ch11/ex11_7.cpp
+++ ch11/ex11_7.cpp
@@ -4,7 +4,6 @@
 // Define a map for which the key is the family’s last name and
 // the value is a vector of the children’s names. Write code to
 // add new families and to add new children to an existing family.
-//  Discussion on Stack Overflow:
 //
 
 #include <iostream>
