2f760e4ba68a226c00825380f6fd398fdd8a2a91
Yue Wang <Moophyalan@gmail.com>
2015-10-23 18:54:13 +1300

    Update ex11_23.cpp

--- ch11/ex11_23.cpp
+++ ch11/ex11_23.cpp
@@ -4,18 +4,22 @@
 //
 //  Created by pezy on 12/16/14.
 //
-//  Rewrite the map that stored vectors of children’s names with a key that is the family last name for the exercises in 11.2.1 (p. 424) to use a multimap.
+//  Rewrite the map that stored vectors of children’s names with a key that is the family last name for the exercises
+//  in 11.2.1 (p. 424) to use a multimap.
 
 #include <map>
 #include <string>
 #include <iostream>
 
 using std::string;
+using std::multimap;
+using std::cin;
+using std::endl;
 
 int main()
 {
-    std::multimap<string, string> families;
-    for (string lastName, childName; std::cin >> childName >> lastName; families.emplace(lastName, childName));
-    for (const auto &s : families)
-        std::cout << s.second << " " << s.first << std::endl;
+    multimap<string, string> families;
+    for (string lname, cname; cin >> cname >> lname; families.emplace(lname, cname));
+    for (auto const& family : families)
+        std::cout << family.second << " " << family.first << endl;
 }
