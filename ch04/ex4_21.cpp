0246cd0328d32c926008049807bb3ba00b374a48
sanshai <415988436@qq.com>
2015-11-25 09:37:32 +0800

    Update ex4_21.cpp

--- ch04/ex4_21.cpp
+++ ch04/ex4_21.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <vector>
 
+using std::cout;
+using std::endl;
+using std::vector;
+
 int main()
 {
-    std::vector<int> ivec{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    for (auto &i : ivec)
-        i = (i % 2) ? (i * 2) : i;
+    vector<int> ivec{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-    //Check
-    for (auto i : ivec)
-        std::cout << i << " ";
-    std::cout << std::endl;
-
+    for (auto i : ivec) {
+        cout << ((i & 0x1) ? i * 2 : i) << " ";
+    }
+    cout << endl;
+    
     return 0;
 }
