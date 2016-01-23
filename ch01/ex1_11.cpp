3e12b9bcb3b57383034f012a7c32975bdd565c40
sawer-you <278475736@qq.com>
2016-01-23 11:58:40 +0800

    Update ex1_11.cpp

--- ch01/ex1_11.cpp
+++ ch01/ex1_11.cpp
@@ -7,10 +7,10 @@ using std::cin;
 
 void print_range(int lo, int hi)
 {
-	if (lo > hi) 
+	if (lo > hi) {
 		print_range(hi, lo);
-		
-	
+		return;
+	}
     for (int i = lo; i != hi; ++i)
         cout << i << " ";
 }
