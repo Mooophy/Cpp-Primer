731b3c7407e9b3c66c9774f7e3ceb84f7c84e02d
Soyn <w604408482@163.com>
2016-02-28 22:18:50 +0800

    fix

--- ch10/ex10_30.cpp
+++ ch10/ex10_30.cpp
@@ -17,8 +17,7 @@ int main()
 {
     std::istream_iterator<int> in_iter(std::cin), eof;
     std::vector<int> vec;
-    while (in_iter != eof)
-        vec.push_back(*in_iter++);
+    std :: copy(in_iter, eof, back_inserter(vec));
     std::sort(vec.begin(), vec.end());
     std::copy(vec.cbegin(), vec.cend(), std::ostream_iterator<int>(std::cout, " "));
 }
