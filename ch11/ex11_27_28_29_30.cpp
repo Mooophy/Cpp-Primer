450bb0f8f578d55cd1bcea0e323b74c2b64c25b1
Soyn <w604408482@163.com>
2016-03-12 19:09:43 +0800

    fix

--- ch11/ex11_27_28_29_30.cpp
+++ ch11/ex11_27_28_29_30.cpp
@@ -42,10 +42,8 @@ int main()
 {
     std::map<std::string, std::vector<int>> m{ { "Alan",{ 1,2,3,4,5, } },{ "John",{ 1,5,6,7,8 } } };
     // ex11.28
-    std::map<std::string, std::vector<int>>::iterator it;
+    std::map<std::string, std::vector<int>>::iterator it = m.find("Alan");
     // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    // type used to define this iterator.
-    it = m.find("Alan");
 
     return 0;
 }
