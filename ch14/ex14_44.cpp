9b6442cf0dd1c0e9ee730d2128cc1eea2c0ed46a
BinTang <tangbin90@users.noreply.github.com>
2016-04-05 20:00:45 +0800

    Ignore invalid input
    put cin into the while statement to avoid any invalid input which may cause run time error

--- ch14/ex14_44.cpp
+++ ch14/ex14_44.cpp
@@ -32,12 +32,13 @@ auto binops = std::map<std::string, std::function<int(int, int)>>
 
 int main()
 {
-    while ( std::cout << "Pls enter as: num operator num :\n", true )
-    {
-        int lhs, rhs; std::string op;
-        std::cin >> lhs >> op >> rhs;
-        std::cout << binops[op](lhs, rhs) << std::endl;
-    }
-
+	std::cout << "Pls enter as: num operator num :\n";
+	int lhs, rhs; std::string op;
+	while (cin >> lhs >> op >> rhs)
+	{
+		std::cout << binops[op](lhs, rhs) << std::endl;
+		std::cout << "Pls enter as: num operator num :\n" ;
+	}
+	system("pause");
     return 0;
 }
