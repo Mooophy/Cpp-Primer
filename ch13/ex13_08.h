ba81d275b4ede69d75bf1b98576f040b493106cb
Soyn <w604408482@163.com>
2016-04-30 17:24:31 +0800

    add the exception safety solution for copy-assignment operator

--- ch13/ex13_08.h
+++ ch13/ex13_08.h
@@ -7,7 +7,7 @@
 //  Write the assignment operator for the HasPtr class from exercise 13.5 in 13.1.1 (p. 499).
 //  As with the copy constructor, your assignment operator should copy the object to which ps points.
 //
-//  See ex13_05.h 
+//  See ex13_05.h
 
 #ifndef CP5_ex13_08_h
 #define CP5_ex13_08_h
@@ -18,11 +18,18 @@ class HasPtr {
 public:
     HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) { }
     HasPtr(const HasPtr &hp) : ps(new std::string(*hp.ps)), i(hp.i) { }
-    HasPtr& operator=(const HasPtr &hp) {
-        std::string *new_ps = new std::string(*hp.ps);
-        delete ps;
-        ps = new_ps;
-        i = hp.i;
+    HasPtr& operator=(const HasPtr &rhs_hp) {
+        if(this != &rhs_hp){  // there is no need to assign for
+                              // the same instance.
+
+            HasPtr temp_HasPtr(rhs_hp);
+            std :: string *temp_ps = temp_HasPtr.ps;
+            temp_HasPtr.ps = ps;
+            ps = temp_ps;
+
+            i = temp_HasPtr.i;
+        }  // temp_HasPtr's destructor will be called,
+           // the memory allocated by original instance will be freed.
         return *this;
     }
 private:
