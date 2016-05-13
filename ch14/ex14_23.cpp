3c680a82eed06703e9d508da2e3e3cd2975df0a6
clarelsy <sanyingli@gmail.com>
2016-05-12 23:13:21 -0400

    Update ex14_23.cpp

--- ch14/ex14_23.cpp
+++ ch14/ex14_23.cpp
@@ -147,8 +147,8 @@ bool operator>=(const StrVec &lhs, const StrVec &rhs)
 StrVec& StrVec::operator=(std::initializer_list<std::string> il)
 {
     auto data = alloc_n_copy(il.begin(), il.end());
-	free();
-	elements = data.first;
-	first_free = cap = data.second;
+    free();
+    elements = data.first;
+    first_free = cap = data.second;
     return *this;
 }
