0c1b140d89887f82f1e24c7f60f43ccb4dcb1495
gzc1003 <gzc1003@sina.com>
2017-04-26 14:13:25 +0800

    better to use size_type
    better to use size_type for subscript/index instead of int

--- ch03/ex3_17.cpp
+++ ch03/ex3_17.cpp
@@ -23,7 +23,7 @@ int main()
     for (string word; cin >> word; vec.push_back(word));
     for (auto &str : vec) for (auto &c : str) c = toupper(c);
 
-    for (int i = 0; i != vec.size(); ++i)
+    for (string::size_type i = 0; i != vec.size(); ++i)
     {
         if (i != 0 && i % 8 == 0) cout << endl;
         cout << vec[i] << " ";
