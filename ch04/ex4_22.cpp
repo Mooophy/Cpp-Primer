46f55315666b6d5a4d0c8e01e195ce28ce2a49b1
CharlieGearsTech <33494513+CharlieGearsTech@users.noreply.github.com>
2018-01-03 16:48:09 -0600

    Update ex4_22.cpp

--- ch04/ex4_22.cpp
+++ ch04/ex4_22.cpp
@@ -3,26 +3,16 @@ using std::cout; using std::cin; using std::endl;
 
 int main()
 {
-//    Exercise 4.22: Extend the program that assigned high pass, pass, and fail
-//    grades to also assign low pass for grades between 60 and 75 inclusive. Write
-//    two versions: One version that uses only conditional operators; the other
-//    should use one or more if statements. Which version do you think is easier
-//    to understand and why?
-    
+    //conditional operator
     for(unsigned int grade=0;cin>>grade;){
         string res=(grade<60)?
                   "fail":grade>=60 && grade<=75?
                               "low pass":(grade>90)?"high pass":"pass";
         cout<<res<<endl;;
     }
-
-    return 0;
     
     for (unsigned g; cin >> g; )
     {
-        // conditional operators
-        auto result = g > 90 ? "high pass" : g < 60 ? "fail" : g < 75 ? "low pass" : "pass";
-        cout << result << endl;
 
         // if statements
         if (g > 90)         cout << "high pass";
