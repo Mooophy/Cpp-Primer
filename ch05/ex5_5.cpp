391431e6e70da589c9f67055ad7af8808728c591
CharlieGearsTech <33494513+CharlieGearsTech@users.noreply.github.com>
2018-01-04 12:20:27 -0600

    Move cout out
    "F" cannot be displayed because cout is inside if-else scope

--- ch05/ex5_5.cpp
+++ ch05/ex5_5.cpp
@@ -6,20 +6,17 @@ using std::vector; using std::string; using std::cout; using std::endl; using st
 int main()
 {
     vector<string> scores = { "F", "D", "C", "B", "A", "A++" };
-    for (int g; cin >> g;)
-    {
+    for(int g; cin>>g;){
         string letter;
-        if (g < 60)
-        {
-            letter = scores[0];
+        if(g<60)
+            letter=scores[0];
+        else{
+            letter=scores[(g-50)/10];
+            if(g != 100)
+                letter+=(g%10>7)?"+":(g%10<3)?"-":"";
         }
-        else
-        {
-            letter = scores[(g - 50) / 10];
-            if (g != 100)
-                letter += g % 10 > 7 ? "+" : g % 10 < 3 ? "-" : "";
-            cout << letter << endl;
-        }
+        cout<<letter<<endl;
+    }}
     }
 
     return 0;
