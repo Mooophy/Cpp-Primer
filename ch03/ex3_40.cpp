e4c84c280409cff6c70731150a7bcb009fa6a425
Jerome <jrong.ece05g@nctu.edu.tw>
2018-06-25 00:54:53 +0800

    Revise error of using constexpr

--- ch03/ex3_40.cpp
+++ ch03/ex3_40.cpp
@@ -1,20 +1,19 @@
 // more discuss: https://github.com/Mooophy/Cpp-Primer/pull/241
-// @frank67
 
 #include <iostream>
 #include <cstring>
 
-const char cstr1[]="Hello";
-const char cstr2[]="world!";
+int main() {
+  const char cStr1[]{"Hello"};
+  const char cStr2[]{"world"};
+  const size_t new_size = strlen(cStr1) + strlen(" ") + strlen(cStr2) + 1;
 
-int main()
-{
-    constexpr size_t new_size = strlen(cstr1) + strlen(" ") + strlen(cstr2) +1;
-    char cstr3[new_size];
-    
-    strcpy(cstr3, cstr1);
-    strcat(cstr3, " ");
-    strcat(cstr3, cstr2);
-    
-    std::cout << cstr3 << std::endl;
+  char cStr3[new_size];
+
+  strcpy(cStr3, cStr1);
+  strcat(cStr3, " ");
+  strcat(cStr3, cStr2);
+
+  std::cout << cStr3 << std::endl;
+  return 0;
 }
