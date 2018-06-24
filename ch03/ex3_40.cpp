641a0621e0e4f66f9890674d9859657d4fd7c9b8
Jerome <jerome9369@gmail.com>
2018-06-25 01:00:48 +0800

    Update constexpr version
    constexpr variable must be initialized by a constant expression.
    This should be concerned as used constexpr a size.

--- ch03/ex3_40.cpp
+++ ch03/ex3_40.cpp
@@ -8,6 +8,11 @@ int main() {
   const char cStr2[]{"world"};
   const size_t new_size = strlen(cStr1) + strlen(" ") + strlen(cStr2) + 1;
 
+  /** CONSTEXPR ver.
+   *  constexpr char cStr1[]{"Hello"};
+   *  constexpr char cStr2[]{"world"};
+   *  constexpr size_t new_size = __builtin_strlen(cStr1) + __builtin_strlen(" ") + __builtin_strlen(cStr2) + 1;
+   **/
   char cStr3[new_size];
 
   strcpy(cStr3, cStr1);
