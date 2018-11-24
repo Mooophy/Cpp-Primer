bf09ff24967a354ed469f32dfb60a12737c8caa1
Bishal Agarwal.B <44868884+b1shal711@users.noreply.github.com>
2018-11-24 21:46:08 +0530

    using gets
    gets is a more basic tool than getline ......want the readers to learn gets before getline

--- ch03/ex3_10.cpp
+++ ch03/ex3_10.cpp
@@ -1,19 +1,10 @@
 //reads a string of characters including punctuation and writes what was read but with the punctuation removed.
 
-#include <iostream>
-#include <string>
-
-using std::string;
-using std::cout;
-using std::cin;
-using std::endl;
+#include <stdio>
 
 int main()
 {
-    cout << "Enter a string of characters including punctuation." << endl;
-    for (string s; getline(cin, s); cout << endl)
-        for (auto i : s) 
-            if (!ispunct(i)) cout << i;
-
+    char a[200];
+    gets(a);
     return 0;
 }
