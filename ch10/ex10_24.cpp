9cdb9ec8e1c20ec474859ba7719d92091e715d46
vhawk19 <varunhawk19@gmail.com>
2019-01-14 12:35:23 +0530

    Update ex10_24.cpp

--- ch10/ex10_24.cpp
+++ ch10/ex10_24.cpp
@@ -33,7 +33,7 @@ int main()
     vector<int> vec{ 0, 1, 2, 3, 4, 5, 6, 7 };
     string str("123456");
     auto result = find_if(vec.begin(), vec.end(), bind(check_size, str, std::placeholders::_1));
-    if (result != vec.cend())
+    if (result != vec.end())
         cout << *result << endl;
     else
         cout << "Not found" << endl;
