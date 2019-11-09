a362ac39085a5cab78672f1c91c5754bcf9de60c
zdb424395031 <39407623+zdb424395031@users.noreply.github.com>
2019-11-09 17:03:06 +0800

    Update ex5_6.cpp

--- ch05/ex5_6.cpp
+++ ch05/ex5_6.cpp
@@ -6,12 +6,12 @@ using std::vector; using std::string; using std::cout; using std::endl; using st
 int main()
 {
     vector<string> scores={"F","D","C","B","A","A++"};
-    int score;
-    while (cin >> score)
+    int grade;
+    while (cin >> grade)
     {
-        string lettergrade = score > 100 ? "score should be less than 100!" : score > 60 ? scores[(score-50)/10] :
+        string lettergrade = grade > 100 ? "score should be less than 100!" : grade > 60 ? scores[(score-50)/10] :
          score >= 0 ? scores[0] : "score should higher than 0!" ;
-        lettergrade += (score >=100 || score <0 || score < 60)? "" : score % 10 > 7? "+" : score % 10 < 3? "-": "" ;
+        lettergrade += (grade >=100 || grade <0 || grade < 60)? "" : grade % 10 > 7? "+" : grade % 10 < 3? "-": "" ;
         cout<<letterband<<endl;
     }
 }
