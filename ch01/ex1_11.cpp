854a0cc1b753a59150eb789f5ff355aa9d78525f
Ch1ldr3n <34335870+Ch1ldr3n@users.noreply.github.com>
2020-07-19 12:07:53 +0800

    Update ex1_11.cpp

--- ch01/ex1_11.cpp
+++ ch01/ex1_11.cpp
@@ -1,26 +1,23 @@
 // Print each number in the range specified by two integers.
 
 #include <iostream>
-
 using std::cout;
 using std::cin;
 
-void print_range(int lo, int hi)
-{
-    if (lo > hi)
-    {
-        print_range(hi, lo);
-        return;
-    }
-    for (int i = lo; i != hi; ++i)
-        cout << i << " ";
+void print_range(int low, int high) {
+	if(low > high) {
+		print_range(high, low);
+		return;
+	}
+	for (int i = low; i <= high; ++i) {
+		cout << i << " ";
+	}
 }
 
-int main()
-{
-    int low = 0, high = 0;
-    cout << "please input two integers:\n";
-    cin >> low >> high;
-    print_range(low, high);
-    return 0;
+int main() {
+	int low = 0, high = 0;
+	cout << "Enter two integers:\n";
+	cin >> low >> high;
+	print_range(low, high);
+	return 0;
 }
