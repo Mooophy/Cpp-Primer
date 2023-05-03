7671e83d0d2255d43a8d838606f2b2ae799adbb8
kevintamakuwala <kevintamakuwala16@gmail.com>
2023-05-03 10:58:51 +0530

    Fixing Infinite loop problem.

--- ch03/ex3_4b.cpp
+++ ch03/ex3_4b.cpp
@@ -3,20 +3,20 @@
 #include <iostream>
 #include <string>
 
-using std::string;
 using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
 
 int main()
 {
-    for (string str1, str2; cin >> str1 >> str2;/* */)
-    {
-        if (str1.size() == str2.size())
-            cout << "The two strings have the same length." << endl;
-        else
-            cout << "The longer string is " << ((str1.size() > str2.size()) ? str1 : str2) << endl;
-    }
+    string str1, str2;
+    cin >> str1 >> str2;
+    
+    if (str1.size() == str2.size())
+        cout << "The two strings have the same length." << endl;
+    else
+        cout << "The longer string is " << ((str1.size() > str2.size()) ? str1 : str2) << endl;
 
     return 0;
 }
